e1068d0e498d4af5cf65750a913fa043cb36005c
Tianle Huang <tianleh@amazon.com>
2021-11-29 18:38:08 -0800

    small wording
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

--- integtest.sh
+++ integtest.sh
@@ -16,7 +16,7 @@ function usage() {
     echo -e "-p BIND_PORT\t, defaults to 9200 or 5601 depends on OpenSearch or Dashboards, can be changed to any port for the cluster location."
     echo -e "-s SECURITY_ENABLED\t(true | false), defaults to true. Specify the OpenSearch/Dashboards have security enabled or not."
     echo -e "-c CREDENTIAL\t(usename:password), no defaults, effective when SECURITY_ENABLED=true."
-    echo -e "-v VERSION\t, no defaults, indicates the OpenSearch version."
+    echo -e "-v VERSION\t, no defaults, indicates the OpenSearch version to test."
     echo -e "-h\tPrint this message."
     echo "--------------------------------------------------------------------------"
 }
