d8e15f6397a3a8e0203fe97e230ae63545f1a206
Tengda-He <89666799+Tengda-He@users.noreply.github.com>
2021-12-02 22:01:36 -0800

    add sanity test for discover and visualize and other pages (#48)
    * refactor test files structure
    
    Signed-off-by: Tengda He <tengh@amazon.com>
    
    * double the timeout threshold to tolerate slow docker env
    
    Signed-off-by: Tengda He <tengh@amazon.com>
    
    * disable home:welcome:show key to local storage
    
    Signed-off-by: Tengda He <tengh@amazon.com>
    
    * add sanity test for discover and visualize page
    
    Signed-off-by: Tengda He <tengh@amazon.com>

--- cypress/integration/opensearch-dashboards/without-security/dashboard_sanity_test_spec.js
+++ cypress/integration/opensearch-dashboards/without-security/dashboard_sanity_test_spec.js
@@ -76,9 +76,68 @@ describe('dashboard sample data validation', () => {
 
     })
 
+    describe('checking Visualize', () => {
+        before(() => {
+            // Go to the Visualize page
+            miscUtils.visitPage('app/visualize#/')
+        })
+
+        after(() => {
+
+        })
+
+        it('checking visualizations list display', () => {
+            commonUI.checkElementExists('div[data-test-subj="itemsInMemTable"]', 1)
+        })
+
+        it('checking search bar display', () => {
+            commonUI.checkElementExists('input[placeholder="Search..."]', 1)
+        })
+
+        it('checking create visualization button display', () => {
+            commonUI.checkElementExists('button[data-test-subj="newItemButton"]', 1)
+        })
+    })
+
+    describe('checking discover', () => {
+        before(() => {
+            // Go to the Discover page
+            miscUtils.visitPage('app/discover#/')
+        })
+
+        after(() => {
+
+        })
+
+        it('checking save query button display', () => {
+            commonUI.checkElementExists('button[data-test-subj="saved-query-management-popover-button"]', 1)
+        })
+
+        it('checking query input display', () => {
+            commonUI.checkElementExists('textarea[data-test-subj="queryInput"]', 1)
+        })
+
+        it('checking refresh button display', () => {
+            commonUI.checkElementExists('button[data-test-subj="querySubmitButton"]', 1)
+        })
+
+        it('checking add filter button display', () => {
+            commonUI.checkElementExists('button[data-test-subj="addFilter"]', 1)
+        })
+
+        it('checking index pattern switch button display', () => {
+            commonUI.checkElementExists('button[data-test-subj="indexPattern-switch-link"]', 1)
+        })
+
+        it('checking field filter display', () => {
+            commonUI.checkElementExists('button[data-test-subj="toggleFieldFilterButton"]', 1)
+        })
+
+    })
+
     describe('adding sample data', () => {
         before(() => {
-            miscUtils.addSampleData(120000)
+            miscUtils.addSampleData()
         })
 
         after(() => {
@@ -101,6 +160,112 @@ describe('dashboard sample data validation', () => {
             miscUtils.viewData('logs')
             commonUI.checkElementContainsValue('span[title="\[Logs\] Web Traffic"]', 1, '\\[Logs\\] Web Traffic')
             commonUI.checkElementContainsValue('div[data-test-subj="markdownBody"] > h3', 1, 'Sample Logs Data')
+        })
+
+        describe('checking index patterns', () => {
+            before(() => {
+                miscUtils.visitPage('app/management/opensearch-dashboards/indexPatterns')
+            })
+    
+            after(() => {
+    
+            })
+    
+            it('checking ecommerce index patterns are added', () => {
+                commonUI.checkElementContainsValue('div[data-test-subj="indexPatternTable"]', 1, 'opensearch_dashboards_sample_data_ecommerce')
+            })
+    
+            it('checking flights index patterns are added', () => {
+                commonUI.checkElementContainsValue('div[data-test-subj="indexPatternTable"]', 1, 'opensearch_dashboards_sample_data_flights')
+            })
+    
+            it('checking web logs index patterns are added', () => {
+                commonUI.checkElementContainsValue('div[data-test-subj="indexPatternTable"]', 1, 'opensearch_dashboards_sample_data_logs')
+            })
+    
+        })
+
+        describe('checking saved objects', () => {
+            before(() => {
+                miscUtils.visitPage('app/management/opensearch-dashboards/objects')
+            })
+    
+            after(() => {
+    
+            })
+    
+            it('checking ecommerce object is saved', () => {
+                commonUI.checkElementContainsValue('div[data-test-subj="savedObjectsTable"]', 1, 'opensearch_dashboards_sample_data_ecommerce')
+            })
+    
+            it('checking flights object is saved', () => {
+                commonUI.checkElementContainsValue('div[data-test-subj="savedObjectsTable"]', 1, 'opensearch_dashboards_sample_data_flights')
+            })
+    
+            it('checking web logs object is saved', () => {
+                commonUI.checkElementContainsValue('div[data-test-subj="savedObjectsTable"]', 1, 'opensearch_dashboards_sample_data_logs')
+            })
+    
+        })
+
+    })
+
+    describe('checking Dev Tools', () => {
+        before(() => {
+            // Go to the Dev Tools page
+            miscUtils.visitPage('app/dev_tools#/console')
+        })
+
+        after(() => {
+
+        })
+
+        it('checking welcome panel display', () => {
+            commonUI.checkElementExists('div[data-test-subj="welcomePanel"]', 1)
+        })
+
+        it('checking dismiss button display', () => {
+            commonUI.checkElementExists('button[data-test-subj="help-close-button"]', 1)
+        })
+
+        it('checking console input area display', () => {
+            commonUI.checkElementExists('div[data-test-subj="request-editor"]', 1)
+        })
+
+        it('checking console output area display', () => {
+            commonUI.checkElementExists('div[data-test-subj="response-editor"]', 1)
+        })
+
+    })
+
+    describe('checking stack management', () => {
+        before(() => {
+            // Go to the stack management page
+            miscUtils.visitPage('app/management/')
+        })
+
+        after(() => {
+
+        })
+
+        it('checking Stack Management display', () => {
+            // Check that Stack Management home is visable
+            commonUI.checkElementExists('div[data-test-subj="managementHome"]', 1)
+        })
+
+        it('checking index patterns link display', () => {
+            // Check that index patterns link is visable
+            commonUI.checkElementExists('a[data-test-subj="indexPatterns"]', 1)
+        })
+
+        it('checking saved objects link display', () => {
+            // Check that saved objects link is visable
+            commonUI.checkElementExists('a[data-test-subj="objects"]', 1)
+        })
+
+        it('checking advance settings link display', () => {
+            // Check that advance settings link is visable
+            commonUI.checkElementExists('a[data-test-subj="settings"]', 1)
         })
 
     })
