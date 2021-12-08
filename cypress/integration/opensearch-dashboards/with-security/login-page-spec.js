be0161212b583b733264f89eaaaf1323b571a562
Tengda-He <89666799+Tengda-He@users.noreply.github.com>
2021-12-07 21:34:57 -0800

    move sanity tests to function to allow import and reuse (#57)
    * refactor test files structure
    
    Signed-off-by: Tengda He <tengh@amazon.com>
    
    * double the timeout threshold to tolerate slow docker env
    
    Signed-off-by: Tengda He <tengh@amazon.com>
    
    * disable home:welcome:show key to local storage
    
    Signed-off-by: Tengda He <tengh@amazon.com>
    
    * add sanity test for discover and visualize page
    
    Signed-off-by: Tengda He <tengh@amazon.com>
    
    * checking discover tests after adding sample data
    
    Signed-off-by: Tengda He <tengh@amazon.com>
    
    * move sample data tests to function for reuse
    
    Signed-off-by: Tengda He <tengh@amazon.com>

--- cypress/integration/opensearch-dashboards/with-security/login-page-spec.js
+++ cypress/integration/opensearch-dashboards/with-security/login-page-spec.js
@@ -1,13 +1,16 @@
 import { LoginPage } from '@opensearch-dashboards-test/opensearch-dashboards-test-library'
+import { dashboardSanityTests } from '../../common/dashboard_sample_data_spec.js'
 
 const loginPage = new LoginPage(cy);
 
 describe('login into security enabled cluster', () => {
     beforeEach(() => {
-        cy.visit('/');
+        Cypress.Cookies.preserveOnce('security_authentication')
     })
 
     it('type user name and password to login', () => {
+        cy.visit('/');
+
         loginPage.enterUserName(Cypress.env('userName'))
 
         loginPage.enterPassword(Cypress.env('password'))
@@ -16,4 +19,6 @@ describe('login into security enabled cluster', () => {
 
         cy.url().should('include', '/app/home#/')
     })
+
+    dashboardSanityTests()
 })
