3fc22dcdfc0297d68d089f5e649d6ed98b140fe0
Tyler Ohlsen <ohltyler@amazon.com>
2021-12-14 15:25:02 -0800

    Add tests for AD plugin; add workflow for plugins
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>

---
+++ cypress/utils/constants.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+// Base URL path with the trailing slash removed
+export const BASE_PATH = new URL(Cypress.config().baseUrl).pathname.replace(
+  /\/$/,
+  ""
+);
+
+/**
+ *****************************
+ AD PLUGIN CONSTANTS
+ *****************************
+ */
+export const BASE_AD_PATH = BASE_PATH + "/app/anomaly-detection-dashboards#";
+
+export const BASE_AD_DASHBOARDS_PATH = BASE_AD_PATH + "/dashboard";
+
+export const BASE_AD_NODE_API_PATH = BASE_PATH + "/api/anomaly_detectors";
+
+export const AD_GET_DETECTORS_NODE_API_PATH =
+  BASE_AD_NODE_API_PATH + "/detectors*";
+
+export const AD_GET_INDICES_NODE_API_PATH =
+  BASE_AD_NODE_API_PATH + "/_indices*";
