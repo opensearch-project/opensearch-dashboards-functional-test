755b6b60683f34ea331b68bf5c749e8d6446459c
Tyler Ohlsen <ohltyler@amazon.com>
2022-02-10 10:07:14 -0800

    Move over remaining AD tests; clean up cypress run scripts (#89)
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -6,6 +6,9 @@
 import {
   AD_GET_DETECTORS_NODE_API_PATH,
   AD_GET_INDICES_NODE_API_PATH,
+  getADStartDetectorNodeApiPath,
+  getADStopDetectorNodeApiPath,
+  AD_GET_MAPPINGS_NODE_API_PATH,
 } from '../utils/constants';
 
 const ADMIN_AUTH = {
@@ -79,12 +82,68 @@ Cypress.Commands.add(
 Cypress.Commands.add(
   'mockSearchIndexOnAction',
   function (fixtureFileName, funcMockedOn) {
-    cy.route2(buildAdApiUrl(AD_GET_INDICES_NODE_API_PATH + '*'), {
+    cy.route2(AD_GET_INDICES_NODE_API_PATH, {
       fixture: fixtureFileName,
     }).as('getIndices');
 
     funcMockedOn();
 
     cy.wait('@getIndices');
+  }
+);
+
+Cypress.Commands.add(
+  'mockStartDetectorOnAction',
+  function (fixtureFileName, detectorId, funcMockedOn) {
+    cy.server();
+    cy.route2(getADStartDetectorNodeApiPath(detectorId), {
+      fixture: fixtureFileName,
+    }).as('startDetector');
+
+    funcMockedOn();
+
+    cy.wait('@startDetector');
+  }
+);
+
+Cypress.Commands.add(
+  'mockStopDetectorOnAction',
+  function (fixtureFileName, detectorId, funcMockedOn) {
+    cy.server();
+    cy.route2(getADStopDetectorNodeApiPath(detectorId), {
+      fixture: fixtureFileName,
+    }).as('stopDetector');
+
+    funcMockedOn();
+
+    cy.wait('@stopDetector');
+  }
+);
+
+Cypress.Commands.add(
+  'mockGetIndexMappingsOnAction',
+  function (fixtureFileName, funcMockedOn) {
+    cy.server();
+    cy.route2(AD_GET_MAPPINGS_NODE_API_PATH, {
+      fixture: fixtureFileName,
+    }).as('getMappings');
+
+    funcMockedOn();
+
+    cy.wait('@getMappings');
+  }
+);
+
+Cypress.Commands.add(
+  'mockCreateDetectorOnAction',
+  function (fixtureFileName, funcMockedOn) {
+    cy.server();
+    cy.route2(AD_GET_DETECTORS_NODE_API_PATH, { fixture: fixtureFileName }).as(
+      'createDetector'
+    );
+
+    funcMockedOn();
+
+    cy.wait('@createDetector');
   }
 );
--- cypress/utils/constants.js
+++ cypress/utils/constants.js
@@ -3,20 +3,23 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// Base URL path with the trailing slash removed
-export const BASE_PATH = new URL(Cypress.config().baseUrl).pathname.replace(
-  /\/$/,
-  ''
-);
+export const BASE_PATH = Cypress.config('baseUrl');
 
 /**
  *****************************
  AD PLUGIN CONSTANTS
  *****************************
  */
+
+export const AD_FIXTURE_BASE_PATH = 'plugins/anomaly-detection-dashboards-plugin/'
+
 export const BASE_AD_PATH = BASE_PATH + '/app/anomaly-detection-dashboards#';
 
 export const BASE_AD_DASHBOARDS_PATH = BASE_AD_PATH + '/dashboard';
+
+export const BASE_AD_DETECTOR_LIST_PATH = BASE_AD_PATH + '/detectors';
+
+export const BASE_AD_CREATE_AD_PATH = BASE_AD_PATH + '/create-ad';
 
 export const BASE_AD_NODE_API_PATH = BASE_PATH + '/api/anomaly_detectors';
 
@@ -25,3 +28,25 @@ export const AD_GET_DETECTORS_NODE_API_PATH =
 
 export const AD_GET_INDICES_NODE_API_PATH =
   BASE_AD_NODE_API_PATH + '/_indices*';
+
+export const AD_GET_MAPPINGS_NODE_API_PATH =
+  BASE_AD_NODE_API_PATH + '/_mappings*';
+
+export function getADStartDetectorNodeApiPath(detectorId) {
+  return BASE_AD_NODE_API_PATH + '/detectors/' + detectorId + '/start';
+}
+
+export function getADStopDetectorNodeApiPath(detectorId) {
+  return BASE_AD_NODE_API_PATH + '/detectors/' + detectorId + '/stop';
+}
+
+export const TEST_DETECTOR_ID = 'ulgqpXEBqtadYz9j2MHG';
+
+// TODO: when repo is onboarded to typescript we can convert these detector states into an enum
+export const DETECTOR_STATE_DISABLED = 'Stopped';
+
+export const DETECTOR_STATE_INIT = 'Initializing';
+
+export const DETECTOR_STATE_RUNNING = 'Running';
+
+export const DETECTOR_STATE_FEATURE_REQUIRED = 'Feature required';
