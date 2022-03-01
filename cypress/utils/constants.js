b85e9c209940c2bcc2ec5f3ff2d2690dfb843399
Darshit Chanpura <dchanp@amazon.com>
2022-02-28 21:34:27 -0500

    Adds extra constants for paths related to internal users page
    Signed-off-by: Darshit Chanpura <dchanp@amazon.com>

--- cypress/utils/constants.js
+++ cypress/utils/constants.js
@@ -69,26 +69,40 @@ export const SEC_TENANTS_FIXTURES_PATH = SEC_FIXTURES_BASE_PATH + '/tenants';
 
 // UI PATHS
 export const BASE_SEC_UI_PATH = '/app/security-dashboards-plugin#';
+
 export const SEC_UI_AUTH_PATH = BASE_SEC_UI_PATH + '/auth';
+
 export const SEC_UI_ROLES_PATH = BASE_SEC_UI_PATH + '/roles';
 export const SEC_UI_ROLES_CREATE_PATH = SEC_UI_ROLES_PATH + '/create';
+
 export const SEC_UI_INTERNAL_USERS_PATH = BASE_SEC_UI_PATH + '/users';
-export const SEC_UI_USER_EDIT_PATH = BASE_SEC_UI_PATH + '/users/edit/';
-export const SEC_UI_USER_DUPLICATE_PATH = BASE_SEC_UI_PATH + '/users/duplicate/';
+export const SEC_UI_USER_EDIT_PATH = SEC_UI_INTERNAL_USERS_PATH + '/edit';
+export const SEC_UI_USER_CREATE_PATH = SEC_UI_INTERNAL_USERS_PATH + '/create';
+export const SEC_UI_USER_DUPLICATE_PATH = SEC_UI_INTERNAL_USERS_PATH + '/duplicate';
+
 export const SEC_UI_PERMISSIONS_PATH = BASE_SEC_UI_PATH + '/permissions';
+
 export const SEC_UI_TENANTS_PATH = BASE_SEC_UI_PATH + '/tenants';
+
 export const SEC_UI_AUDIT_LOGGING_PATH = BASE_SEC_UI_PATH + '/auditLogging';
 export const SEC_UI_AUDIT_LOGGING_EDIT_PATH = SEC_UI_AUDIT_LOGGING_PATH + '/edit/complianceSettings';
 
 // API PATHS
 export const BASE_SEC_API_PATH = '/api/v1/configuration';
+
 export const SEC_API_CONFIG_PATH = BASE_SEC_API_PATH + '/securityconfig';
+
 export const SEC_API_ROLES_PATH = BASE_SEC_API_PATH + '/roles';
 export const SEC_API_ROLESMAPPING_PATH = BASE_SEC_API_PATH + '/rolesmapping';
+
 export const SEC_API_ACTIONGROUPS_PATH = BASE_SEC_API_PATH + '/actiongroups';
+
 export const SEC_API_TENANTS_PATH = BASE_SEC_API_PATH + '/tenants';
+
 export const SEC_API_INTERNAL_USERS_PATH = BASE_SEC_API_PATH + '/internalusers';
+
 export const SEC_API_ACCOUNT_PATH = BASE_SEC_API_PATH + '/account';
+
 export const SEC_API_AUDIT_PATH = BASE_SEC_API_PATH + '/audit';
 export const SEC_API_AUDIT_CONFIG_PATH = SEC_API_AUDIT_PATH + '/config';
 
