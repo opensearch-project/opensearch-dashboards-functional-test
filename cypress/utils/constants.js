2b8f0508e330257a61cb2b12dc244b3e1e04c4ac
David Cui <davidcui@amazon.com>
2022-03-02 13:50:05 -0800

    add query workbench to dashboards functional test suite
    Signed-off-by: David Cui <davidcui@amazon.com>

--- cypress/utils/constants.js
+++ cypress/utils/constants.js
@@ -6,4 +6,5 @@
 export * from './base_constants';
 export * from './plugins/anomaly-detection-dashboards-plugin/constants';
 export * from './plugins/index-management-dashboards-plugin/constants';
+export * from './plugins/query-workbench-dashboards/constants';
 export * from './plugins/security/constants';
