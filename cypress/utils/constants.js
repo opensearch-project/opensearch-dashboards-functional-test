eb241030afedebbf6231df89f9b7cbbba6d9343f
David Cui <davidcui@amazon.com>
2022-03-02 10:50:05 -0800

    fix reference in constants to reports-dashboards
    Signed-off-by: David Cui <davidcui@amazon.com>

--- cypress/utils/constants.js
+++ cypress/utils/constants.js
@@ -6,5 +6,5 @@
 export * from './base_constants';
 export * from './plugins/anomaly-detection-dashboards-plugin/constants';
 export * from './plugins/index-management-dashboards-plugin/constants';
-export * from './plugins/index-management-dashboards-plugin/constants';
+export * from './plugins/reports-dashboards/constants';
 export * from './plugins/security/constants';
