1ed8bd57c02771c57f317405b76a6f0512365a9f
David Cui <53581635+davidcui1225@users.noreply.github.com>
2022-03-04 10:16:37 -0800

    Add Reporting cypress tests (#96)
    * add cypress testing for reporting
    
    Signed-off-by: David Cui <davidcui@amazon.com>
    
    * extract sample data out of cypress tests, remove after test suite done, remove duplicate constant and increase timeout for adding sample data
    
    Signed-off-by: David Cui <davidcui@amazon.com>
    
    * simplify constants reference
    
    Signed-off-by: David Cui <davidcui@amazon.com>
    
    * fix reference in constants to reports-dashboards
    
    Signed-off-by: David Cui <davidcui@amazon.com>
    
    * increase timeouts to try to fix flaky tests
    
    Signed-off-by: David Cui <davidcui@amazon.com>
    
    * increase timeout threshold to 50 seconds
    
    Signed-off-by: David Cui <davidcui@amazon.com>
    
    * last try for increasing timeout threshold to 60s
    
    Signed-off-by: David Cui <davidcui@amazon.com>
    
    * test extracting command out of test function and reduce threshold from being unneccessarily high
    
    Signed-off-by: David Cui <davidcui@amazon.com>
    
    * fix recurring interval tests and reduce timeout threshold
    
    Signed-off-by: David Cui <davidcui@amazon.com>

--- cypress/utils/constants.js
+++ cypress/utils/constants.js
@@ -6,5 +6,6 @@
 export * from './base_constants';
 export * from './plugins/anomaly-detection-dashboards-plugin/constants';
 export * from './plugins/index-management-dashboards-plugin/constants';
+export * from './plugins/reports-dashboards/constants';
 export * from './plugins/query-workbench-dashboards/constants';
 export * from './plugins/security/constants';
