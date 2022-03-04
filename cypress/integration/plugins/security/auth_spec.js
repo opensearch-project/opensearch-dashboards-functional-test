4ec0a3ba598e142b4f6d2a5f674f1899cb9f8ee1
Tianle Huang <tianleh@amazon.com>
2022-03-04 14:51:27 -0800

    skip security related tests if security disabled
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

--- cypress/integration/plugins/security/auth_spec.js
+++ cypress/integration/plugins/security/auth_spec.js
@@ -8,48 +8,50 @@ import {
   SEC_FIXTURES_BASE_PATH
 } from '../../../utils/constants';
 
-describe('Authc and Authz page', () => {
-  // start a server so that server responses can be mocked via fixtures
-  // in all of the below test cases
-  before(() => {
-    cy.server();
-  });
-
-  it('authentication and authorization section should exist', () => {
-    cy.mockAuthAction(
-      SEC_FIXTURES_BASE_PATH + '/auth_response.json',
-      () => {
-        cy.visit(SEC_UI_AUTH_PATH);
-      }
-    );
-
-    cy.contains('h3', 'Authentication sequences');
-    cy.contains('span', 'kerberos_auth_domain');
-
-    cy.contains('h3', 'Authorization');
-    cy.contains('span', 'roles_from_another_ldap');
-  });
-
-  it('View Expression Modal should display and close correctly', () => {
-    cy.mockAuthAction(
-      SEC_FIXTURES_BASE_PATH + '/auth_response.json',
-      () => {
-        cy.visit(SEC_UI_AUTH_PATH);
-      }
-    );
-
-    cy.get('.euiModal').should('not.exist');
+if(Cypress.env("SECURITY_ENABLED")) {
+  describe('Authc and Authz page', () => {
+    // start a server so that server responses can be mocked via fixtures
+    // in all of the below test cases
+    before(() => {
+      cy.server();
+    });
   
-    cy.get('[data-test-subj=view-expression]')
-      .first()
-      .click({ force: true });
-
-    cy.get('.euiModal').should('be.visible');
-
-    cy.get('.euiModal__closeIcon').click({ force: true });
-
-    cy.get('.euiModal').should('not.exist');
-
+    it('authentication and authorization section should exist', () => {
+      cy.mockAuthAction(
+        SEC_FIXTURES_BASE_PATH + '/auth_response.json',
+        () => {
+          cy.visit(SEC_UI_AUTH_PATH);
+        }
+      );
+  
+      cy.contains('h3', 'Authentication sequences');
+      cy.contains('span', 'kerberos_auth_domain');
+  
+      cy.contains('h3', 'Authorization');
+      cy.contains('span', 'roles_from_another_ldap');
+    });
+  
+    it('View Expression Modal should display and close correctly', () => {
+      cy.mockAuthAction(
+        SEC_FIXTURES_BASE_PATH + '/auth_response.json',
+        () => {
+          cy.visit(SEC_UI_AUTH_PATH);
+        }
+      );
+  
+      cy.get('.euiModal').should('not.exist');
+    
+      cy.get('[data-test-subj=view-expression]')
+        .first()
+        .click({ force: true });
+  
+      cy.get('.euiModal').should('be.visible');
+  
+      cy.get('.euiModal__closeIcon').click({ force: true });
+  
+      cy.get('.euiModal').should('not.exist');
+  
+    });
+  
   });
-
-});
+}
