df68010a4e994f37e2adc5632d756235a8718dbb
Kawika Avilla <kavilla414@gmail.com>
2022-04-26 20:42:04 +0000

    bump up time
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>

--- cypress/utils/plugins/observability-dashboards/constants.js
+++ cypress/utils/plugins/observability-dashboards/constants.js
@@ -241,7 +241,7 @@ export const moveToCreatePage = () => {
 export const moveToApplication = (name) => {
   cy.visit(`${BASE_PATH}/app/observability-dashboards#/application_analytics/`);
   supressResizeObserverIssue();
-  cy.wait(delayTime * 6);
+  cy.wait(delayTime * 7);
   cy.contains(name, { timeout: 120000 }).click();
   cy.wait(delayTime * 2);
   cy.get('.euiTitle--large', { timeout: 60000 })
