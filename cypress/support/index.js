68340d0331333e8fabdc5253329df82cdb3b4379
CCongWang <wangcong@umich.edu>
2022-04-30 06:17:49 +0000

    observability: fix ResizeObserver loop limit exceeded error
    Signed-off-by: CCongWang <wangcong@umich.edu>

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -23,7 +23,7 @@ import '../utils/plugins/alerting-dashboards-plugin/commands';
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-const resizeObserverLoopErrRe = /^[^(ResizeObserver loop limit exceeded)]/;
+const resizeObserverLoopErrRe = /^ResizeObserver loop limit exceeded/;
 Cypress.on('uncaught:exception', (err) => {
   /* returning false here prevents Cypress from failing the test */
   if (resizeObserverLoopErrRe.test(err.message)) {
