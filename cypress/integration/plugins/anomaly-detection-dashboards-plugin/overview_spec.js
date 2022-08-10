efbffc5c1a70053fb78c17bd495c36a51d8bab2f
Jackie Han <jkhanjob@gmail.com>
2022-08-10 09:24:22 -0700

    catch flaky tests
    Signed-off-by: Jackie Han <jkhanjob@gmail.com>

--- cypress/integration/plugins/anomaly-detection-dashboards-plugin/overview_spec.js
+++ cypress/integration/plugins/anomaly-detection-dashboards-plugin/overview_spec.js
@@ -38,8 +38,8 @@ context('Overview page', () => {
 
   it('Empty dashboard redirects to overview page', () => {
     cy.visit(AD_URL.DETECTOR_LIST);
-    cy.get('[data-test-subj=detectorListHeader]').then(($body) => {
-      if ($body.find("[data-test-subj=detectorListHeader]").contains('(0)')) {  
+    cy.get('[data-test-subj=emptyDetectorListMessage]').then(($body) => {
+      if ($body.find("button[data-cy=sampleDetectorButton]").length > 0) {  
         cy.visit(AD_URL.DASHBOARD);
         cy.get('[data-test-subj=emptyDetectorListMessage]').then(($body) => {
           if ($body.find("button[data-cy=sampleDetectorButton]").length > 0) {  
@@ -53,8 +53,8 @@ context('Overview page', () => {
 
   it('Empty detector list redirects to overview page', () => {
     cy.visit(AD_URL.DETECTOR_LIST);
-    cy.get('[data-test-subj=detectorListHeader]').then(($body) => {
-      if ($body.find("[data-test-subj=detectorListHeader]").contains('(0)')) {
+    cy.get('[data-test-subj=emptyDetectorListMessage]').then(($body) => {
+      if ($body.find("button[data-cy=sampleDetectorButton]").length > 0) {  
         cy.getElementByTestId('sampleDetectorButton').click();
         validatePageElements();
       }
