af2c42428522189469e92778b4ccb7101ffc6d2e
mpabba3003 <amazonmanideep@gmail.com>
2022-10-18 09:45:39 -0700

    functional tests for datasource management listing
    Signed-off-by: mpabba3003 <amazonmanideep@gmail.com>

---
+++ cypress/utils/dashboards/datasource-management-dashboards-plugin/commands.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { BASE_PATH } from '../../base_constants';
+import { DS_API } from './constants';
+
+Cypress.Commands.add('deleteAllDataSources', () => {
+  // Clean all data sources
+  cy.request('GET', `${BASE_PATH}${DS_API.DATA_SOURCES_LISTING}`).then(
+    (resp) => {
+      if (resp && resp.body && resp.body.saved_objects) {
+        resp.body.saved_objects.map(({ id }) => {
+          cy.request({
+            method: 'DELETE',
+            url: `${BASE_PATH}${DS_API.DELETE_DATA_SOURCE}${id}`,
+            body: { force: false },
+            headers: {
+              'osd-xsrf': true,
+            },
+          });
+        });
+      }
+    }
+  );
+});
+
+Cypress.Commands.add('createDataSource', (dataSourceJSON) => {
+  cy.request({
+    method: 'POST',
+    url: `${BASE_PATH}${DS_API.CREATE_DATA_SOURCE}`,
+    headers: {
+      'osd-xsrf': true,
+    },
+    body: dataSourceJSON,
+  });
+});
+
+Cypress.Commands.add(
+  'getColumnHeaderByNameAndClickForSorting',
+  (identifier, columnHeaderName) => {
+    cy.get(identifier).contains(columnHeaderName).click({ force: true });
+  }
+);
