15705e6b987e9b25eff0daae332352ddbf8e10bb
AWSHurneyt <hurneyt@amazon.com>
2022-11-08 01:06:42 -0500

    Onboarding security analytics dashboards plugin. (#380)
    * Added security analytics dashboards.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Fixed url is visit() call.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Enabled github action on PRs to 2.x branch.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * Adjusted workflow to run without security.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    * clean up commands
    
    Signed-off-by: Kawika Avilla <avillk@amazon.com>
    
    * Enabled security for onboarding experimental plugin.
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    
    Signed-off-by: AWSHurneyt <hurneyt@amazon.com>
    Signed-off-by: Kawika Avilla <avillk@amazon.com>
    Co-authored-by: Kawika Avilla <avillk@amazon.com>
    (cherry picked from commit 165f8559cca46f141fe9460463b6793ca4b14ad5)

---
+++ cypress/utils/plugins/security-analytics-dashboards-plugin/constants.js
@@ -0,0 +1,31 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export const TWENTY_SECONDS_TIMEOUT = { timeout: 20000 };
+
+export const INDICES = {
+  DETECTORS_INDEX: '.opensearch-detectors-config',
+  PRE_PACKAGED_RULES_INDEX: '.opensearch-pre-packaged-rules-config',
+  CUSTOM_RULES_INDEX: '.opensearch-custom-rules-config',
+};
+
+export const PLUGIN_NAME = 'opensearch_security_analytics_dashboards';
+export const BASE_API_PATH = '/_plugins/_security_analytics';
+
+export const NODE_API = {
+  DETECTORS_BASE: `${BASE_API_PATH}/detectors`,
+  SEARCH_DETECTORS: `${BASE_API_PATH}/detectors/_search`,
+  INDICES_BASE: `${BASE_API_PATH}/indices`,
+  GET_FINDINGS: `${BASE_API_PATH}/findings/_search`,
+  DOCUMENT_IDS_QUERY: `${BASE_API_PATH}/document_ids_query`,
+  TIME_RANGE_QUERY: `${BASE_API_PATH}/time_range_query`,
+  MAPPINGS_BASE: `${BASE_API_PATH}/mappings`,
+  MAPPINGS_VIEW: `${BASE_API_PATH}/mappings/view`,
+  GET_ALERTS: `${BASE_API_PATH}/alerts`,
+  RULES_BASE: `${BASE_API_PATH}/rules`,
+  CHANNELS: `${BASE_API_PATH}/_notifications/channels`,
+  ACKNOWLEDGE_ALERTS: `${BASE_API_PATH}/detectors/{detector_id}/_acknowledge/alerts`,
+  INDEX_TEMPLATE_BASE: '/_index_template',
+};
