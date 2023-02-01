325dd8dd8f8c8d2aca3cee21657aedebc1d1469a
Kawika Avilla <kavilla414@gmail.com>
2023-02-01 14:02:12 -0800

    Clean up linter issues (#454)
    Clean up linter issues with:
    `yarn lint --fix`
    
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>
    (cherry picked from commit 82c44737cb07523edc32b988cf03850e8e7a1b48)

--- cypress/integration/plugins/security-analytics-dashboards-plugin/rules_spec.js
+++ cypress/integration/plugins/security-analytics-dashboards-plugin/rules_spec.js
@@ -3,13 +3,17 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { PLUGIN_NAME, TWENTY_SECONDS_TIMEOUT } from '../../../utils/plugins/security-analytics-dashboards-plugin/constants'
-import {BASE_PATH} from "../../../utils/base_constants";
+import {
+  PLUGIN_NAME,
+  TWENTY_SECONDS_TIMEOUT,
+} from '../../../utils/plugins/security-analytics-dashboards-plugin/constants';
+import { BASE_PATH } from '../../../utils/base_constants';
 
 const SAMPLE_RULE = {
   name: 'Cypress test rule',
   logType: 'windows',
-  description: 'This is a rule used to test the rule creation workflow. Not for production use.',
+  description:
+    'This is a rule used to test the rule creation workflow. Not for production use.',
   detection:
     'selection:\n  Provider_Name: Service Control Manager\nEventID: 7045\nServiceName: ZzNetSvc\n{backspace}{backspace}condition: selection',
   detectionLine: [
@@ -20,7 +24,11 @@ const SAMPLE_RULE = {
     'condition: selection',
   ],
   severity: 'critical',
-  tags: ['attack.persistence', 'attack.privilege_escalation', 'attack.t1543.003'],
+  tags: [
+    'attack.persistence',
+    'attack.privilege_escalation',
+    'attack.t1543.003',
+  ],
   references: 'https://nohello.com',
   falsePositive: 'unknown',
   author: 'Cypress Test Runner',
@@ -40,32 +48,41 @@ describe('Rules', () => {
   describe('Can be created', () => {
     it('manually using UI', () => {
       // Click "create new rule" button
-      cy.get('[data-test-subj="create_rule_button"]', TWENTY_SECONDS_TIMEOUT).click({
+      cy.get(
+        '[data-test-subj="create_rule_button"]',
+        TWENTY_SECONDS_TIMEOUT
+      ).click({
         force: true,
       });
 
       // Enter the name
-      cy.get('[data-test-subj="rule_name_field"]', TWENTY_SECONDS_TIMEOUT).type(SAMPLE_RULE.name);
+      cy.get('[data-test-subj="rule_name_field"]', TWENTY_SECONDS_TIMEOUT).type(
+        SAMPLE_RULE.name
+      );
 
       // Enter the log type
-      cy.get('[data-test-subj="rule_type_dropdown"]', TWENTY_SECONDS_TIMEOUT).select(
-        SAMPLE_RULE.logType
-      );
+      cy.get(
+        '[data-test-subj="rule_type_dropdown"]',
+        TWENTY_SECONDS_TIMEOUT
+      ).select(SAMPLE_RULE.logType);
 
       // Enter the description
-      cy.get('[data-test-subj="rule_description_field"]', TWENTY_SECONDS_TIMEOUT).type(
-        SAMPLE_RULE.description
-      );
+      cy.get(
+        '[data-test-subj="rule_description_field"]',
+        TWENTY_SECONDS_TIMEOUT
+      ).type(SAMPLE_RULE.description);
 
       // Enter the detection
-      cy.get('[data-test-subj="rule_detection_field"]', TWENTY_SECONDS_TIMEOUT).type(
-        SAMPLE_RULE.detection
-      );
+      cy.get(
+        '[data-test-subj="rule_detection_field"]',
+        TWENTY_SECONDS_TIMEOUT
+      ).type(SAMPLE_RULE.detection);
 
       // Enter the severity
-      cy.get('[data-test-subj="rule_severity_dropdown"]', TWENTY_SECONDS_TIMEOUT).select(
-        SAMPLE_RULE.severity
-      );
+      cy.get(
+        '[data-test-subj="rule_severity_dropdown"]',
+        TWENTY_SECONDS_TIMEOUT
+      ).select(SAMPLE_RULE.severity);
 
       // Enter the tags
       SAMPLE_RULE.tags.forEach((tag) =>
@@ -75,27 +92,34 @@ describe('Rules', () => {
       );
 
       // Enter the reference
-      cy.get('[data-test-subj="rule_references_field_0"]', TWENTY_SECONDS_TIMEOUT).type(
-        SAMPLE_RULE.references
-      );
+      cy.get(
+        '[data-test-subj="rule_references_field_0"]',
+        TWENTY_SECONDS_TIMEOUT
+      ).type(SAMPLE_RULE.references);
 
       // Enter the false positive cases
-      cy.get('[data-test-subj="rule_false_positive_cases_field_0"]', TWENTY_SECONDS_TIMEOUT).type(
-        SAMPLE_RULE.falsePositive
-      );
+      cy.get(
+        '[data-test-subj="rule_false_positive_cases_field_0"]',
+        TWENTY_SECONDS_TIMEOUT
+      ).type(SAMPLE_RULE.falsePositive);
 
       // Enter the author
-      cy.get('[data-test-subj="rule_author_field"]', TWENTY_SECONDS_TIMEOUT).type(
-        SAMPLE_RULE.author
-      );
+      cy.get(
+        '[data-test-subj="rule_author_field"]',
+        TWENTY_SECONDS_TIMEOUT
+      ).type(SAMPLE_RULE.author);
 
       // Enter the log type
-      cy.get('[data-test-subj="rule_status_dropdown"]', TWENTY_SECONDS_TIMEOUT).select(
-        SAMPLE_RULE.status
-      );
+      cy.get(
+        '[data-test-subj="rule_status_dropdown"]',
+        TWENTY_SECONDS_TIMEOUT
+      ).select(SAMPLE_RULE.status);
 
       // Click "create" button
-      cy.get('[data-test-subj="create_rule_button"]', TWENTY_SECONDS_TIMEOUT).click({
+      cy.get(
+        '[data-test-subj="create_rule_button"]',
+        TWENTY_SECONDS_TIMEOUT
+      ).click({
         force: true,
       });
 
@@ -109,23 +133,29 @@ describe('Rules', () => {
         .type(`${SAMPLE_RULE.name}{enter}`);
 
       // Click the rule link to open the details flyout
-      cy.get(`[data-test-subj="rule_link_${SAMPLE_RULE.name}"]`, TWENTY_SECONDS_TIMEOUT).click();
+      cy.get(
+        `[data-test-subj="rule_link_${SAMPLE_RULE.name}"]`,
+        TWENTY_SECONDS_TIMEOUT
+      ).click();
 
       // Confirm the flyout contains the expected values
-      cy.get(`[data-test-subj="rule_flyout_${SAMPLE_RULE.name}"]`, TWENTY_SECONDS_TIMEOUT)
+      cy.get(
+        `[data-test-subj="rule_flyout_${SAMPLE_RULE.name}"]`,
+        TWENTY_SECONDS_TIMEOUT
+      )
         .click({ force: true })
         .within(() => {
           // Validate name
-          cy.get('[data-test-subj="rule_flyout_rule_name"]', TWENTY_SECONDS_TIMEOUT).contains(
-            SAMPLE_RULE.name,
+          cy.get(
+            '[data-test-subj="rule_flyout_rule_name"]',
             TWENTY_SECONDS_TIMEOUT
-          );
+          ).contains(SAMPLE_RULE.name, TWENTY_SECONDS_TIMEOUT);
 
           // Validate log type
-          cy.get('[data-test-subj="rule_flyout_rule_log_type"]', TWENTY_SECONDS_TIMEOUT).contains(
-            SAMPLE_RULE.logType,
+          cy.get(
+            '[data-test-subj="rule_flyout_rule_log_type"]',
             TWENTY_SECONDS_TIMEOUT
-          );
+          ).contains(SAMPLE_RULE.logType, TWENTY_SECONDS_TIMEOUT);
 
           // Validate description
           cy.get(
@@ -134,35 +164,38 @@ describe('Rules', () => {
           ).contains(SAMPLE_RULE.description, TWENTY_SECONDS_TIMEOUT);
 
           // Validate author
-          cy.get('[data-test-subj="rule_flyout_rule_author"]', TWENTY_SECONDS_TIMEOUT).contains(
-            SAMPLE_RULE.author,
+          cy.get(
+            '[data-test-subj="rule_flyout_rule_author"]',
             TWENTY_SECONDS_TIMEOUT
-          );
+          ).contains(SAMPLE_RULE.author, TWENTY_SECONDS_TIMEOUT);
 
           // Validate source is "custom"
-          cy.get('[data-test-subj="rule_flyout_rule_source"]', TWENTY_SECONDS_TIMEOUT).contains(
-            'Custom',
+          cy.get(
+            '[data-test-subj="rule_flyout_rule_source"]',
             TWENTY_SECONDS_TIMEOUT
-          );
+          ).contains('Custom', TWENTY_SECONDS_TIMEOUT);
 
           // Validate severity
-          cy.get('[data-test-subj="rule_flyout_rule_severity"]', TWENTY_SECONDS_TIMEOUT).contains(
-            SAMPLE_RULE.severity,
+          cy.get(
+            '[data-test-subj="rule_flyout_rule_severity"]',
             TWENTY_SECONDS_TIMEOUT
-          );
+          ).contains(SAMPLE_RULE.severity, TWENTY_SECONDS_TIMEOUT);
 
           // Validate tags
           SAMPLE_RULE.tags.forEach((tag) =>
             cy
-              .get('[data-test-subj="rule_flyout_rule_tags"]', TWENTY_SECONDS_TIMEOUT)
+              .get(
+                '[data-test-subj="rule_flyout_rule_tags"]',
+                TWENTY_SECONDS_TIMEOUT
+              )
               .contains(tag, TWENTY_SECONDS_TIMEOUT)
           );
 
           // Validate references
-          cy.get('[data-test-subj="rule_flyout_rule_references"]', TWENTY_SECONDS_TIMEOUT).contains(
-            SAMPLE_RULE.references,
+          cy.get(
+            '[data-test-subj="rule_flyout_rule_references"]',
             TWENTY_SECONDS_TIMEOUT
-          );
+          ).contains(SAMPLE_RULE.references, TWENTY_SECONDS_TIMEOUT);
 
           // Validate false positives
           cy.get(
@@ -171,26 +204,34 @@ describe('Rules', () => {
           ).contains(SAMPLE_RULE.falsePositive, TWENTY_SECONDS_TIMEOUT);
 
           // Validate status
-          cy.get('[data-test-subj="rule_flyout_rule_status"]', TWENTY_SECONDS_TIMEOUT).contains(
-            SAMPLE_RULE.status,
+          cy.get(
+            '[data-test-subj="rule_flyout_rule_status"]',
             TWENTY_SECONDS_TIMEOUT
-          );
+          ).contains(SAMPLE_RULE.status, TWENTY_SECONDS_TIMEOUT);
 
           // Validate detection
           SAMPLE_RULE.detectionLine.forEach((line) =>
             cy
-              .get('[data-test-subj="rule_flyout_rule_detection"]', TWENTY_SECONDS_TIMEOUT)
+              .get(
+                '[data-test-subj="rule_flyout_rule_detection"]',
+                TWENTY_SECONDS_TIMEOUT
+              )
               .contains(line, TWENTY_SECONDS_TIMEOUT)
           );
 
           // Close the flyout
-          cy.get('[data-test-subj="euiFlyoutCloseButton"]', TWENTY_SECONDS_TIMEOUT).click({
+          cy.get(
+            '[data-test-subj="euiFlyoutCloseButton"]',
+            TWENTY_SECONDS_TIMEOUT
+          ).click({
             force: true,
           });
         });
 
       // Confirm flyout closed
-      cy.contains(`[data-test-subj="rule_flyout_${SAMPLE_RULE.name}"]`).should('not.exist');
+      cy.contains(`[data-test-subj="rule_flyout_${SAMPLE_RULE.name}"]`).should(
+        'not.exist'
+      );
     });
   });
 
