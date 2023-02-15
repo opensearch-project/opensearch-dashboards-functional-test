46dda8322e0f6ae9417ed6fd9100a194d4bd6260
Derek Ho <derek01778@gmail.com>
2023-02-15 17:10:27 -0500

    fix time zone for trace analytics test (#529)
    * fix time zone
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    * make more general for any time zone
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    
    ---------
    
    Signed-off-by: Derek Ho <dxho@amazon.com>
    (cherry picked from commit 4c70c1a484247bd4b14eed68ee3edd98d40ea88c)
    Signed-off-by: Derek Ho <dxho@amazon.com>

--- cypress/integration/plugins/observability-dashboards/3_trace_analytics_traces.spec.js
+++ cypress/integration/plugins/observability-dashboards/3_trace_analytics_traces.spec.js
@@ -11,6 +11,10 @@ import {
   SPAN_ID,
   TRACE_ID,
 } from '../../../utils/constants';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import dayjs from 'dayjs';
+
+dayjs.extend(customParseFormat);
 
 describe('Testing traces table empty state', () => {
   beforeEach(() => {
@@ -40,7 +44,12 @@ describe('Testing traces table', () => {
 
   it('Renders the traces table', () => {
     cy.contains(' (108)').should('exist');
-    cy.contains('03/25/2021 10:23:45').should('exist');
+    cy.get('.euiTableCellContent')
+      .eq(17)
+      .invoke('text')
+      .then((text) => {
+        expect(dayjs(text, 'MM/DD/YYYY HH:mm:ss', true).isValid()).to.be.true;
+      });
     cy.contains('03f9c770db5ee2f1caac0...').should('exist');
     cy.contains('224.99').should('exist');
 
@@ -60,7 +69,12 @@ describe('Testing traces table', () => {
     cy.get('input[type="search"]').focus().type(`${TRACE_ID}{enter}`);
     cy.get('.euiButton__text').contains('Refresh').click();
     cy.contains(' (1)').should('exist');
-    cy.contains('03/25/2021 10:21:22').should('exist');
+    cy.get('.euiTableCellContent')
+      .eq(11)
+      .invoke('text')
+      .then((text) => {
+        expect(dayjs(text, 'MM/DD/YYYY HH:mm:ss', true).isValid()).to.be.true;
+      });
   });
 });
 
