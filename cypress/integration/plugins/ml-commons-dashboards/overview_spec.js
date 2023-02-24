6db10b9895dc16e81a0c5616b07d056da7c9577b
Lin Wang <wonglam@amazon.com>
2023-02-24 09:06:33 +0800

    Add experimental flag for ml commons dashboards (#567)
    * Add ML_COMMONS_DASHBOARDS_ENABLED to control ml-commons-dashboards releated tests
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * Add ml_commons_dashboards.enabled=true to workflow files
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * Add CYPRESS_ML_COMMONS_DASHBOARDS_ENABLED=true to workflow files
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    * Fix Unexpected value 'env'
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    
    ---------
    
    Signed-off-by: Lin Wang <wonglam@amazon.com>
    (cherry picked from commit afe79f5c7538ecb04f4f30cbcf9836fa58a2f2c6)

--- cypress/integration/plugins/ml-commons-dashboards/overview_spec.js
+++ cypress/integration/plugins/ml-commons-dashboards/overview_spec.js
@@ -4,94 +4,96 @@
  */
 import { MLC_URL } from '../../../utils/constants';
 
-describe('MLC Overview page', () => {
-  let uploadedModelId;
-  let uploadedModelLoadedError;
-  const uploadModelName = `traced_small_model-${new Date()
-    .getTime()
-    .toString(34)}`;
-  before(() => {
-    // Disable only_run_on_ml_node to avoid model upload error in case of cluster no ml nodes
-    cy.disableOnlyRunOnMLNode();
-    cy.wait(1000);
+if (Cypress.env('ML_COMMONS_DASHBOARDS_ENABLED')) {
+  describe('MLC Overview page', () => {
+    let uploadedModelId;
+    let uploadedModelLoadedError;
+    const uploadModelName = `traced_small_model-${new Date()
+      .getTime()
+      .toString(34)}`;
+    before(() => {
+      // Disable only_run_on_ml_node to avoid model upload error in case of cluster no ml nodes
+      cy.disableOnlyRunOnMLNode();
+      cy.wait(1000);
 
-    cy.uploadModelByUrl({
-      name: uploadModelName,
-      version: '1.0.0',
-      model_format: 'TORCH_SCRIPT',
-      model_task_type: 'text_embedding',
-      model_config: {
-        model_type: 'bert',
-        embedding_dimension: 768,
-        framework_type: 'sentence_transformers',
-        all_config:
-          '{"architectures":["BertModel"],"max_position_embeddings":512,"model_type":"bert","num_attention_heads":12,"num_hidden_layers":6}',
-      },
-      url: 'https://github.com/opensearch-project/ml-commons/blob/2.x/ml-algorithms/src/test/resources/org/opensearch/ml/engine/algorithms/text_embedding/traced_small_model.zip?raw=true',
-    })
-      .then(({ task_id: taskId }) =>
-        cy.cyclingCheckTask({
-          taskId,
-        })
-      )
-      .then(({ model_id: modelId }) => {
-        uploadedModelId = modelId;
-        return cy.loadMLCommonsModel(modelId);
+      cy.uploadModelByUrl({
+        name: uploadModelName,
+        version: '1.0.0',
+        model_format: 'TORCH_SCRIPT',
+        model_task_type: 'text_embedding',
+        model_config: {
+          model_type: 'bert',
+          embedding_dimension: 768,
+          framework_type: 'sentence_transformers',
+          all_config:
+            '{"architectures":["BertModel"],"max_position_embeddings":512,"model_type":"bert","num_attention_heads":12,"num_hidden_layers":6}',
+        },
+        url: 'https://github.com/opensearch-project/ml-commons/blob/2.x/ml-algorithms/src/test/resources/org/opensearch/ml/engine/algorithms/text_embedding/traced_small_model.zip?raw=true',
       })
-      .then(({ task_id: taskId }) =>
-        cy.cyclingCheckTask({
-          taskId,
-          rejectOnError: false,
+        .then(({ task_id: taskId }) =>
+          cy.cyclingCheckTask({
+            taskId,
+          })
+        )
+        .then(({ model_id: modelId }) => {
+          uploadedModelId = modelId;
+          return cy.loadMLCommonsModel(modelId);
         })
-      )
-      .then(({ error }) => {
-        if (error) {
-          uploadedModelLoadedError = error;
-        }
-      });
+        .then(({ task_id: taskId }) =>
+          cy.cyclingCheckTask({
+            taskId,
+            rejectOnError: false,
+          })
+        )
+        .then(({ error }) => {
+          if (error) {
+            uploadedModelLoadedError = error;
+          }
+        });
+    });
+
+    after(() => {
+      if (uploadedModelId) {
+        cy.unloadMLCommonsModel(uploadedModelId);
+        cy.deleteMLCommonsModel(uploadedModelId);
+      }
+    });
+
+    it('should return to monitoring page when visit root', () => {
+      cy.visit(MLC_URL.ROOT);
+      cy.url().should('include', MLC_URL.OVERVIEW);
+    });
+
+    it('should display page header and deployed model name, status and id', () => {
+      cy.visit(MLC_URL.OVERVIEW);
+
+      cy.contains('h1', 'Overview');
+      cy.contains('h2', 'Deployed models');
+
+      cy.get('[aria-label="Search by name or ID"]').type(uploadModelName);
+
+      cy.contains(uploadedModelId)
+        .closest('tr')
+        .contains(uploadedModelLoadedError ? 'Not responding' : 'Responding')
+        .closest('tr')
+        .contains(uploadModelName);
+
+      cy.contains('h1', 'Overview');
+      cy.contains('h2', 'Deployed models');
+    });
+
+    it('should open preview panel after view detail button click', () => {
+      cy.visit(MLC_URL.OVERVIEW);
+
+      cy.get('[aria-label="Search by name or ID"]').type(uploadModelName);
+
+      cy.contains(uploadedModelId)
+        .closest('tr')
+        .find('[aria-label="view detail"]')
+        .click();
+
+      cy.contains('.euiFlyoutHeader > h3', uploadModelName);
+      cy.contains('.euiFlyoutBody', uploadedModelId);
+    });
   });
-
-  after(() => {
-    if (uploadedModelId) {
-      cy.unloadMLCommonsModel(uploadedModelId);
-      cy.deleteMLCommonsModel(uploadedModelId);
-    }
-  });
-
-  it('should return to monitoring page when visit root', () => {
-    cy.visit(MLC_URL.ROOT);
-    cy.url().should('include', MLC_URL.OVERVIEW);
-  });
-
-  it('should display page header and deployed model name, status and id', () => {
-    cy.visit(MLC_URL.OVERVIEW);
-
-    cy.contains('h1', 'Overview');
-    cy.contains('h2', 'Deployed models');
-
-    cy.get('[aria-label="Search by name or ID"]').type(uploadModelName);
-
-    cy.contains(uploadedModelId)
-      .closest('tr')
-      .contains(uploadedModelLoadedError ? 'Not responding' : 'Responding')
-      .closest('tr')
-      .contains(uploadModelName);
-
-    cy.contains('h1', 'Overview');
-    cy.contains('h2', 'Deployed models');
-  });
-
-  it('should open preview panel after view detail button click', () => {
-    cy.visit(MLC_URL.OVERVIEW);
-
-    cy.get('[aria-label="Search by name or ID"]').type(uploadModelName);
-
-    cy.contains(uploadedModelId)
-      .closest('tr')
-      .find('[aria-label="view detail"]')
-      .click();
-
-    cy.contains('.euiFlyoutHeader > h3', uploadModelName);
-    cy.contains('.euiFlyoutBody', uploadedModelId);
-  });
-});
+}
