6456e27d89d7ef5358f25428061a50a4b168d0ce
suzhou <suzhou@amazon.com>
2023-04-07 14:16:46 +0800

    feat: add cypress test for 2.7
    Signed-off-by: suzhou <suzhou@amazon.com>

---
+++ cypress/integration/plugins/index-management-dashboards-plugin/force_merge.js
@@ -0,0 +1,88 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import {
+  IM_PLUGIN_NAME,
+  BASE_PATH,
+  BACKEND_BASE_PATH,
+} from '../../../utils/constants';
+
+const rolloverValidAlias = 'rollover-valid-alias';
+const rolloverAliasNeedTargetIndex = 'rollover-alias-need-target-index';
+const rolloverDataStream = 'data-stream-rollover';
+const validIndex = 'index-000001';
+const invalidIndex = 'index-test-rollover';
+
+describe('force_merge', () => {
+  before(() => {
+    // Set welcome screen tracking to false
+    localStorage.setItem('home:welcome:show', 'false');
+    cy.deleteTemplate('index-common-template');
+    cy.deleteAllIndices();
+    cy.request({
+      url: `${BACKEND_BASE_PATH}/_data_stream/*`,
+      method: 'DELETE',
+      failOnStatusCode: false,
+    });
+    cy.createIndex(validIndex);
+    cy.createIndex(invalidIndex);
+    cy.addIndexAlias(rolloverValidAlias, validIndex);
+    cy.addIndexAlias(rolloverAliasNeedTargetIndex, invalidIndex);
+    cy.createIndexTemplate('index-common-template', {
+      index_patterns: ['data-stream-*'],
+      data_stream: {},
+      template: {
+        aliases: {
+          alias_for_common_1: {},
+          alias_for_common_2: {},
+        },
+        settings: {
+          number_of_shards: 2,
+          number_of_replicas: 1,
+        },
+      },
+    });
+    cy.request({
+      url: `${BACKEND_BASE_PATH}/_data_stream/${rolloverDataStream}`,
+      method: 'PUT',
+      failOnStatusCode: false,
+    });
+  });
+
+  describe('force merge', () => {
+    it('force merge data stream / index / alias successfully', () => {
+      // Visit ISM OSD
+      cy.visit(`${BASE_PATH}/app/${IM_PLUGIN_NAME}#/force-merge`);
+      cy.contains('Configure source index', { timeout: 60000 });
+
+      // click create
+      cy.get('[data-test-subj="forceMergeConfirmButton"]').click({
+        force: true,
+      });
+
+      cy.contains('Index or data stream is required.');
+      cy.get(
+        '[data-test-subj="sourceSelector"] [data-test-subj="comboBoxSearchInput"]'
+      ).type(
+        `${rolloverValidAlias}{downArrow}{enter}${rolloverAliasNeedTargetIndex}{downArrow}{enter}${rolloverDataStream}{downArrow}{enter}${validIndex}{downArrow}{enter}${invalidIndex}{downArrow}{enter}`
+      );
+
+      cy.get('[data-test-subj="forceMergeConfirmButton"]').click({
+        force: true,
+      });
+
+      cy.contains(/Some shards could not be force merged/);
+    });
+  });
+
+  after(() => {
+    cy.deleteTemplate('index-common-template');
+    cy.deleteAllIndices();
+    cy.request({
+      url: `${BACKEND_BASE_PATH}/_data_stream/*`,
+      method: 'DELETE',
+      failOnStatusCode: false,
+    });
+  });
+});
