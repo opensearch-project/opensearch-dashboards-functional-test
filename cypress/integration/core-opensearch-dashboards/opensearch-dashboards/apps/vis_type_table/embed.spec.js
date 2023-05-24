f83f287498b9fc759c8a3269f1d9b6e43abd26ff
Anan Zhuang <ananzh@amazon.com>
2023-05-24 14:16:45 -0700

    [Table visualization] Add functional tests for new table vis (#423)
    * [Table visualization] Add functional tests for new table vis
    
    There are four test cases:
    * basic.spec.js contains tests related to vis page, like
    inspector, save an object.
    * data.spec.js tests diff metrics and buckets aggregations.
    * options.spec.js contains tests on options panel.
    * split.spec.js tests table functions, like sort, adjust col
    width, split tables and etc.
    
    Most tests are the same as the removed funtional tests.
    split.spec.js is the only one with all new tests.
    
    Partially Resolved:
    https://github.com/opensearch-project/opensearch-dashboards-functional-test/issues/401
    
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>
    
    * fix PR comments
    
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>
    
    * fix test error and add embed test suite
    
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>
    
    * Fix multiple submit bug
    Currently there are two submit type button in data source
    which cause cypress fail to find and click. In this PR, I
    switch to use data-test-subj to uniquely identify the btn.
    
    Issue Resolved:
    https://github.com/opensearch-project/opensearch-dashboards-functional-test/issues/439
    
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>
    
    * missed conflict resolution
    
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>
    
    ---------
    
    Signed-off-by: Anan Zhuang <ananzh@amazon.com>
    Signed-off-by: Kawika Avilla <kavilla414@gmail.com>
    Co-authored-by: Kawika Avilla <kavilla414@gmail.com>
    (cherry picked from commit 3dffb5f4e27e53d168ae29cc061e565c4dcb45c3)

---
+++ cypress/integration/core-opensearch-dashboards/opensearch-dashboards/apps/vis_type_table/embed.spec.js
@@ -0,0 +1,167 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { CommonUI } from '@opensearch-dashboards-test/opensearch-dashboards-test-library';
+import {
+  BASE_PATH,
+  TABLE_INDEX_ID,
+  TABLE_PATH_INDEX_DATA,
+  TABLE_INDEX_PATTERN,
+  TABLE_PATH_SO_DATA,
+  TABLE_BASIC_VIS_TITLE,
+  TABLE_VIS_APP_PATH,
+  TABLE_INDEX_START_TIME,
+  TABLE_INDEX_END_TIME,
+  toTestId,
+} from '../../../../../utils/constants';
+
+const commonUI = new CommonUI(cy);
+
+/**
+ * Embed test suite description:
+ * Sometimes, we might want to embed a visualization in an iframe.
+ * This suite is to test how an embedded table visualization works.
+ * In this test suite, we have the following tests:
+ * Test that a table visualization is loaded properly in embedded mode.
+ * Test that table visualization can be filtered in embedded mode.
+ * Test that table visualization can filter for value. There are two ways
+ * to filter for value. First is to click the filter for button. Second is
+ * to click the expand button then choose fiter for option. We test both ways.
+ * Test that table visualization can filter out value. Same as filter for value.
+ */
+
+describe('table visualization in embedded mode', () => {
+  const expectedData = [
+    '0',
+    '1,059',
+    '20',
+    '2,236',
+    '40',
+    '2,200',
+    '60',
+    '2,211',
+    '80',
+    '2,179',
+    '100',
+    '115',
+  ];
+
+  before(() => {
+    cy.deleteIndex(TABLE_INDEX_ID);
+    cy.deleteIndexPattern(TABLE_INDEX_PATTERN);
+    cy.bulkUploadDocs(TABLE_PATH_INDEX_DATA);
+    cy.importSavedObjects(TABLE_PATH_SO_DATA);
+    // Load table visualization
+    cy.visit(`${BASE_PATH}/app/visualize`);
+    cy.get('input[type="search"]').type(`${TABLE_BASIC_VIS_TITLE}{enter}`);
+    cy.get('.euiBasicTable-loading').should('not.exist'); // wait for the loading to stop
+    cy.getElementByTestId(
+      `visListingTitleLink-${toTestId(TABLE_BASIC_VIS_TITLE)}`
+    )
+      .should('exist')
+      .click();
+    cy.url().should('contain', TABLE_VIS_APP_PATH);
+    cy.setTopNavDate(TABLE_INDEX_START_TIME, TABLE_INDEX_END_TIME);
+    cy.waitForLoader();
+    // Set a Histogram Aggregation
+    // We will use this aggregation for all test case
+    cy.tbAddBucketsAggregation();
+    cy.tbSplitRows();
+    cy.tbSetupHistogramAggregation('age', '20', 2);
+    cy.url().then((url) => {
+      let embedUrl = url + '&embed=true';
+      cy.wrap(embedUrl).as('embedUrl');
+    });
+    cy.get('@embedUrl').then((url) => {
+      expect(url).to.contain('&embed=true');
+      cy.visit(url);
+      cy.reload();
+    });
+  });
+
+  //  after(() => {
+  //    cy.deleteIndex(TABLE_INDEX_ID);
+  //    cy.deleteIndexPattern(TABLE_INDEX_PATTERN);
+  //  });
+
+  it('Should open table vis in embedded mode', () => {
+    cy.tbGetTableDataFromVisualization().then((data) => {
+      expect(data).to.deep.eq(expectedData);
+    });
+  });
+
+  it('Should allow to filter in embedded mode', () => {
+    commonUI.addFilterRetrySelection('age', 'is between', ['10', '30']);
+    cy.reload();
+    cy.tbGetTableDataFromVisualization().then((data) => {
+      expect(data).to.deep.eq(['0', '1,059', '20', '1,114']);
+    });
+    commonUI.removeFilter('age');
+    cy.reload();
+    cy.tbGetTableDataFromVisualization().then((data) => {
+      expect(data).to.deep.eq(expectedData);
+    });
+  });
+
+  it('Should filter for value in embedded mode', () => {
+    cy.tbClickTableCellAction(2, 0, 0, 'filter for', 0, true);
+    cy.reload();
+    cy.tbGetTableDataFromVisualization().then((data) => {
+      expect(data).to.deep.eq(['0', '1,059']);
+    });
+    commonUI.removeFilter('age');
+    cy.reload();
+    cy.tbGetTableDataFromVisualization().then((data) => {
+      expect(data).to.deep.eq(expectedData);
+    });
+    cy.tbClickTableCellAction(2, 0, 0, 'expand', 0, true);
+    cy.tbClickFilterFromExpand('filter for');
+    cy.reload();
+    cy.tbGetTableDataFromVisualization().then((data) => {
+      expect(data).to.deep.eq(['0', '1,059']);
+    });
+    commonUI.removeFilter('age');
+    cy.reload();
+    cy.tbGetTableDataFromVisualization().then((data) => {
+      expect(data).to.deep.eq(expectedData);
+    });
+  });
+
+  it('Should filter out value in embedded mode', () => {
+    const expectedFilterOutData = [
+      '20',
+      '2,236',
+      '40',
+      '2,200',
+      '60',
+      '2,211',
+      '80',
+      '2,179',
+      '100',
+      '115',
+    ];
+    cy.tbClickTableCellAction(2, 0, 0, 'filter out', 0, true);
+    cy.reload();
+    cy.tbGetTableDataFromVisualization().then((data) => {
+      expect(data).to.deep.eq(expectedFilterOutData);
+    });
+    commonUI.removeFilter('age');
+    cy.reload();
+    cy.tbGetTableDataFromVisualization().then((data) => {
+      expect(data).to.deep.eq(expectedData);
+    });
+    cy.tbClickTableCellAction(2, 0, 0, 'expand', 0, true);
+    cy.tbClickFilterFromExpand('filter out');
+    cy.reload();
+    cy.tbGetTableDataFromVisualization().then((data) => {
+      expect(data).to.deep.eq(expectedFilterOutData);
+    });
+    commonUI.removeFilter('age');
+    cy.reload();
+    cy.tbGetTableDataFromVisualization().then((data) => {
+      expect(data).to.deep.eq(expectedData);
+    });
+  });
+});
---
+++ cypress/utils/dashboards/vis_type_table/table.js
@@ -0,0 +1,123 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+Cypress.Commands.add('tbGetAllTableDataFromVisualization', (total) => {
+  let data = [];
+  for (let i = 0; i < total; i++) {
+    data.push([]);
+    cy.get('[class="visTable__group"]')
+      .eq(i)
+      .find('[data-test-subj="dataGridWrapper"]')
+      .find('[data-test-subj="dataGridRowCell"]')
+      .find('[class="euiDataGridRowCell__truncate"]')
+      .find('span')
+      .each(($cell) => data[i].push($cell.text()));
+  }
+  return cy.wrap(data);
+});
+
+Cypress.Commands.add('tbGetTableDataFromVisualization', () => {
+  let data = [];
+  cy.getElementByTestId('dataGridWrapper')
+    .find('[data-test-subj="dataGridRowCell"]')
+    .find('[class="euiDataGridRowCell__truncate"]')
+    .find('span')
+    .each(($cell) => data.push($cell.text()));
+  return cy.wrap(data);
+});
+
+Cypress.Commands.add('tbGetTotalValueFromTable', () => {
+  let data = [];
+  cy.getElementByTestId('dataGridRow')
+    .find('[data-test-subj="dataGridRowCell"]')
+    .find('[class="euiDataGridRowCell__truncate"]')
+    .each(($val) => {
+      const elementText = $val.text();
+      const value = elementText.substring(0, elementText.indexOf('Row'));
+      data.push(value);
+    });
+  return cy.wrap(data);
+});
+
+Cypress.Commands.add('tbSelectSortColumn', (tableIndex, colIndex, dir) => {
+  expect(dir).to.be.oneOf(['asc', 'desc']);
+  const dirIndex = dir == 'asc' ? 0 : 1;
+  cy.get('[class="visTable__group"]')
+    .eq(tableIndex)
+    .find('[class="euiDataGridHeaderCell"]')
+    .eq(colIndex)
+    .find('[class="euiDataGridHeaderCell__button"]')
+    .click();
+  cy.get('[class="euiListGroupItem__button"]').eq(dirIndex).click();
+});
+
+Cypress.Commands.add('tbGetColumnWidth', (tableIndex, colIndex, name) => {
+  cy.get('[class="visTable__group"]')
+    .eq(tableIndex)
+    .find('[data-test-subj="dataGridHeader"]')
+    .find('[class="euiDataGridHeaderCell"]')
+    .eq(colIndex)
+    .invoke('attr', 'style')
+    .then((value) => {
+      const width = value.split(' ').pop();
+      const widthValue = Number(width.substring(0, width.indexOf('p')));
+      cy.wrap(widthValue).as(name);
+    });
+});
+
+Cypress.Commands.add(
+  'tbAdjustColumnWidth',
+  (totalColumn, tableIndex, colIndex, size) => {
+    const resizerIndex = totalColumn * tableIndex + colIndex;
+    cy.getElementByTestId('dataGridColumnResizer')
+      .eq(resizerIndex)
+      .trigger('mousedown', { which: 1, pageX: 0, pageY: 0 })
+      .trigger('mousemove', { which: 1, pageX: size, pageY: 0 })
+      .trigger('mouseup', { force: true });
+  }
+);
+
+Cypress.Commands.add(
+  'tbClickTableCellAction',
+  (totalColumn, rowIndex, colIndex, action, tableIndex = 0, embed = false) => {
+    expect(action).to.be.oneOf(['filter for', 'filter out', 'expand']);
+    const filterFor = '[data-test-subj="filterForValue"]';
+    const filterOut = '[data-test-subj="filterOutValue"]';
+    const expand =
+      '[class="euiButtonIcon euiButtonIcon--primary euiButtonIcon--fill euiButtonIcon--xSmall euiDataGridRowCell__expandButtonIcon"]';
+    const actionButton =
+      action == 'filter for'
+        ? filterFor
+        : action == 'filter out'
+        ? filterOut
+        : expand;
+    if (embed == true) {
+      cy.get('[data-test-subj="dataGridRowCell"]')
+        .eq(rowIndex * totalColumn + colIndex)
+        .click()
+        .find(actionButton)
+        .click({ force: true });
+    } else {
+      cy.get('[class="visTable__group"]')
+        .eq(tableIndex)
+        .find('[data-test-subj="dataGridWrapper"]')
+        .find('[data-test-subj="dataGridRowCell"]')
+        .eq(rowIndex * totalColumn + colIndex)
+        .click()
+        .find(actionButton)
+        .click({ force: true });
+    }
+  }
+);
+
+Cypress.Commands.add('tbClickFilterFromExpand', (action) => {
+  expect(action).to.be.oneOf(['filter for', 'filter out']);
+  const actionButton =
+    action == 'filter for' ? 'filterForValue' : 'filterOutValue';
+  cy.get('[class="euiPopoverFooter"]')
+    .find('[class="euiFlexItem"]')
+    .find(`[data-test-subj=${actionButton}]`)
+    .click();
+});
