d65bf9f772a254ba6e49f64f81f68c5f440a21c3
Tyler Ohlsen <ohltyler@amazon.com>
2023-06-27 15:55:15 -0700

    [Vis Augmenter / Feature Anywhere] Add test suite for vanilla OSD + helper fns for plugins (#725)
    * feature anywhere initial tests
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * Add test suite
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Remove unnecessary test case
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Optimize getters
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    ---------
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    Co-authored-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>

---
+++ cypress/integration/core-opensearch-dashboards/opensearch-dashboards/apps/vis-augmenter/dashboard_spec.js
@@ -0,0 +1,130 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {
+  INDEX_PATTERN_FILEPATH_SIMPLE,
+  INDEX_SETTINGS_FILEPATH_SIMPLE,
+  SAMPLE_DATA_FILEPATH_SIMPLE,
+} from '../../../../../utils/constants';
+import {
+  deleteVisAugmenterData,
+  bootstrapDashboard,
+} from '../../../../../utils/dashboards/vis-augmenter/helpers';
+
+describe('Vis augmenter - existing dashboards work as expected', () => {
+  describe('dashboard with ineligible, eligible, and vega visualizations', () => {
+    const indexName = 'vis-augmenter-sample-index';
+    const indexPatternName = 'vis-augmenter-sample-*';
+    const dashboardName = 'Vis Augmenter Dashboard';
+    const visualizationSpecs = [
+      {
+        name: 'count-agg-vis',
+        type: 'line',
+        indexPattern: indexPatternName,
+        metrics: [],
+      },
+      {
+        name: 'single-metric-vis',
+        type: 'line',
+        indexPattern: indexPatternName,
+        metrics: [
+          {
+            aggregation: 'Average',
+            field: 'value1',
+          },
+        ],
+      },
+      {
+        name: 'multi-metric-vis',
+        type: 'line',
+        indexPattern: indexPatternName,
+        metrics: [
+          {
+            aggregation: 'Average',
+            field: 'value1',
+          },
+          {
+            aggregation: 'Average',
+            field: 'value2',
+          },
+          {
+            aggregation: 'Max',
+            field: 'value3',
+          },
+        ],
+      },
+      {
+        name: 'area-vis',
+        type: 'area',
+        indexPattern: indexPatternName,
+        metrics: [
+          {
+            aggregation: 'Max',
+            field: 'value2',
+          },
+        ],
+      },
+      {
+        name: 'vega-vis',
+        type: 'vega',
+        indexPattern: indexPatternName,
+        metrics: [],
+      },
+    ];
+
+    const visualizationNames = visualizationSpecs.map(
+      (visualizationSpec) => visualizationSpec.name
+    );
+
+    before(() => {
+      // Create a dashboard and add some visualizations
+      bootstrapDashboard(
+        INDEX_SETTINGS_FILEPATH_SIMPLE,
+        INDEX_PATTERN_FILEPATH_SIMPLE,
+        SAMPLE_DATA_FILEPATH_SIMPLE,
+        indexName,
+        indexPatternName,
+        dashboardName,
+        visualizationSpecs
+      );
+    });
+
+    beforeEach(() => {
+      cy.visitDashboard(dashboardName);
+      cy.wait(5000);
+    });
+
+    after(() => {
+      deleteVisAugmenterData(
+        indexName,
+        indexPatternName,
+        visualizationNames,
+        dashboardName
+      );
+    });
+
+    it('View events option does not exist for any visualization', () => {
+      visualizationNames.forEach((visualizationName) => {
+        cy.getVisPanelByTitle(visualizationName)
+          .openVisContextMenu()
+          .getMenuItems()
+          .contains('View Events')
+          .should('not.exist');
+      });
+    });
+
+    it('Validate non-vega visualizations are not rendered with vega under the hood', () => {
+      visualizationSpecs.forEach((visualizationSpec) => {
+        cy.getVisPanelByTitle(visualizationSpec.name).within(() => {
+          if (visualizationSpec.type === 'vega') {
+            cy.get('.vgaVis__view').should('exist');
+          } else {
+            cy.get('.vgaVis__view').should('not.exist');
+          }
+        });
+      });
+    });
+  });
+});
