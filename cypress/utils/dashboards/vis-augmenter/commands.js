50137602da8818ea37e98e7ae812b428f91f26d5
Tyler Ohlsen <ohltyler@amazon.com>
2023-07-07 12:38:16 -0700

    [Feature Anywhere / Vis Augmenter] Add test flows for integration with AD plugin (#727)
    * feature anywhere initial tests
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * Add test suite
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Add AD vis augmenter tests
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * More refactoring
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * More tests
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Add test for AD cleanup scenario
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Set up saved obj test suite
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Add reminder TODO
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Add tests regarding saved obj visibility
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Add view events tests
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * cleanup
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * remove import
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    ---------
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    Co-authored-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>

--- cypress/utils/dashboards/vis-augmenter/commands.js
+++ cypress/utils/dashboards/vis-augmenter/commands.js
@@ -3,7 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import _ from 'lodash';
 import { BASE_PATH } from '../../constants';
 
 Cypress.Commands.add('getVisPanelByTitle', (title) =>
@@ -26,7 +25,6 @@ Cypress.Commands.add(
       .find('button')
       .contains(text)
       .click()
-      .then(() => cy.get('.euiContextMenu'))
 );
 
 Cypress.Commands.add('getMenuItems', { prevSubject: 'optional' }, (menu) =>
@@ -35,9 +33,16 @@ Cypress.Commands.add('getMenuItems', { prevSubject: 'optional' }, (menu) =>
 
 Cypress.Commands.add('visitDashboard', (dashboardName) => {
   cy.visit(`${BASE_PATH}/app/dashboards`);
+  cy.wait(2000);
   cy.get('.euiFieldSearch').type(dashboardName);
-  cy.wait(1000);
+  cy.wait(2000);
   cy.get('[data-test-subj="itemsInMemTable"]').contains(dashboardName).click({
     force: true,
   });
+  cy.wait(5000);
+});
+
+Cypress.Commands.add('visitSavedObjectsManagement', () => {
+  cy.visit(`${BASE_PATH}/app/management/opensearch-dashboards/objects`);
+  cy.wait(5000);
 });
