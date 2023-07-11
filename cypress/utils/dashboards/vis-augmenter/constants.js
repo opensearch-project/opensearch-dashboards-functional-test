43bc5efa31755990f9692fd920964c1e45d277e6
Tyler Ohlsen <ohltyler@amazon.com>
2023-07-11 12:04:25 -0700

    [Vis Augmenter / Feature Anywhere] Add tests in core OSD and AD plugin (#739)
    * [Vis Augmenter / Feature Anywhere] Add test suite for vanilla OSD + helper fns for plugins (#725)
    
    * feature anywhere initial tests
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * Add test suite
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Remove unnecessary test case
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Optimize getters
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    ---------
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    Co-authored-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * [Feature Anywhere / Vis Augmenter] Add test flows for integration with AD plugin (#727)
    
    * feature anywhere initial tests
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * Add test suite
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Add AD vis augmenter tests
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * More refactoring
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * More tests
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Add test for AD cleanup scenario
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Set up saved obj test suite
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Add reminder TODO
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Add tests regarding saved obj visibility
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Add view events tests
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * cleanup
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * remove import
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    ---------
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    Co-authored-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    ---------
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    Co-authored-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    (cherry picked from commit 07a67d724710109bd95800dd5ce973e83dbbe10a)

---
+++ cypress/utils/dashboards/vis-augmenter/constants.js
@@ -0,0 +1,15 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+const SAMPLE_DATA_DIR_SIMPLE =
+  'dashboard/opensearch_dashboards/vis-augmenter/sample-data-simple/';
+
+export const SAMPLE_DATA_FILEPATH_SIMPLE = SAMPLE_DATA_DIR_SIMPLE + 'data.json';
+
+export const INDEX_PATTERN_FILEPATH_SIMPLE =
+  SAMPLE_DATA_DIR_SIMPLE + 'index-pattern-fields.txt';
+
+export const INDEX_SETTINGS_FILEPATH_SIMPLE =
+  SAMPLE_DATA_DIR_SIMPLE + 'index-settings.txt';
