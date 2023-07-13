b848a95fd188a4b7051ec0084d611a61cb47e210
Tyler Ohlsen <ohltyler@amazon.com>
2023-07-13 16:34:42 -0700

    [Vis Augmenter] Add tests in OSD & AD plugin (#752)
    * [Vis Augmenter / Feature Anywhere] Add test suite for vanilla OSD + helper fns for plugins (#725)
    
    * feature anywhere initial tests
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * Add test suite
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Remove unnecessary test case
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Optimize getters
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    ---------
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    Co-authored-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * [Feature Anywhere / Vis Augmenter] Add test flows for integration with AD plugin (#727)
    
    * feature anywhere initial tests
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * Add test suite
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Add AD vis augmenter tests
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * More refactoring
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * More tests
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Add test for AD cleanup scenario
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Set up saved obj test suite
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Add reminder TODO
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Add tests regarding saved obj visibility
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Add view events tests
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * cleanup
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * remove import
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    ---------
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    Co-authored-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    
    * Fix NPE
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Fix bug of non-empty dashboard; remove unnecessary test
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Simplify dashboard creation
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    * Update undefined check
    
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    
    ---------
    
    Signed-off-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    Signed-off-by: Tyler Ohlsen <ohltyler@amazon.com>
    Co-authored-by: Jovan Cvetkovic <jovanca.cvetkovic@gmail.com>
    (cherry picked from commit 39444c1b9be27b193f64ab19b15bdd4e21628658)

--- cypress/integration/plugins/anomaly-detection-dashboards-plugin/sample_detector_spec.js
+++ cypress/integration/plugins/anomaly-detection-dashboards-plugin/sample_detector_spec.js
@@ -3,27 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { AD_URL } from '../../../utils/constants';
+import { createSampleDetector } from '../../../utils/helpers';
 
 context('Sample detectors', () => {
-  // Helper fn that takes in a button test ID to determine
-  // the sample detector to create
-  const createSampleDetector = (createButtonDataTestSubj) => {
-    cy.visit(AD_URL.OVERVIEW);
-
-    cy.getElementByTestId('overviewTitle').should('exist');
-    cy.getElementByTestId('viewSampleDetectorLink').should('not.exist');
-    cy.getElementByTestId(createButtonDataTestSubj).click();
-    cy.visit(AD_URL.OVERVIEW);
-
-    // Check that the details page defaults to real-time, and shows detector is initializing
-    cy.getElementByTestId('viewSampleDetectorLink').click();
-    cy.getElementByTestId('detectorNameHeader').should('exist');
-    cy.getElementByTestId('sampleIndexDetailsCallout').should('exist');
-    cy.getElementByTestId('realTimeResultsHeader').should('exist');
-    cy.getElementByTestId('detectorStateInitializing').should('exist');
-  };
-
   beforeEach(() => {
     cy.deleteAllIndices();
     cy.deleteADSystemIndices();
