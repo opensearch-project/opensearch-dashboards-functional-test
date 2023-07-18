14a870b5fa700f46c3facbb5056532f38d0aa189
leanneeliatra <leanne.laceybyrne@eliatra.com>
2023-07-18 10:50:02 +0100

    Global declaration of the tenant removed. The clearing of session storage is not needed actually, cy.visit() does this for us
    Signed-off-by: leanneeliatra <leanne.laceybyrne@eliatra.com>
    Signed-off-by: leanne.laceybyrne@eliatra.com <leanne.laceybyrne@eliatra.com>

--- cypress/integration/plugins/security-dashboards-plugin/change_tenant_successfully.js
+++ cypress/integration/plugins/security-dashboards-plugin/change_tenant_successfully.js
@@ -4,28 +4,20 @@
  */
 
 import { CURRENT_TENANT } from '../../../utils/commands';
-import tenantDescription from '../../../fixtures/plugins/security-dashboards-plugin/tenants/testTenant.json';
 import { switchTenantTo } from './switch_tenant';
 
-const tenantName = 'tenant';
 
 if (Cypress.env('SECURITY_ENABLED')) {
   describe('Switch tenants when visiting copied links: ', () => {
+    const tenantName = 'private';
+
     before(() => {
       cy.server();
-
-      cy.createTenant(tenantName, tenantDescription);
     });
     it('Checks that the tenant switcher can switch tenants despite a different tenant being present in the tenant query parameter.', function () {
       CURRENT_TENANT.newTenant = tenantName;
 
-      cy.visit('/app/home', {
-        // Clean up
-        onBeforeLoad() {
-          window.localStorage.clear();
-          window.sessionStorage.clear();
-        },
-      }).then(() => {
+      cy.visit('/app/home').then(() => {
         cy.waitForLoader();
         switchTenantTo('global');
         cy.waitForLoader();
