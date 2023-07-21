6d74e00de863596e5ad50ad15e222a4f4d2ff4e6
SuZhou-Joe <suzhou@amazon.com>
2023-07-21 10:18:51 +0800

    feat: sync cypress test from notifications-dashboards (#776)
    * feat: sync cypress test from notifications-dashboards
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: rename commands
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: update
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: add larger wait time
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: update
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: update
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: update
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    * feat: add refresh before delete all notifications configs
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    
    ---------
    
    Signed-off-by: SuZhou-Joe <suzhou@amazon.com>
    (cherry picked from commit 81f70c37fde7b468915b173b0e81f4a609ecea84)

--- cypress/utils/plugins/notifications-dashboards/constants.js
+++ cypress/utils/plugins/notifications-dashboards/constants.js
@@ -5,3 +5,7 @@
 
 export const NOTIFICATIONS_PLUGIN_NAME = 'notifications-dashboards';
 export const NOTIFICATIONS_DELAY = 1000;
+export const NOTIFICATIONS_API_ROUTE_PREFIX = '/_plugins/_notifications';
+export const API = {
+  CONFIGS_BASE: `${NOTIFICATIONS_API_ROUTE_PREFIX}/configs`,
+};
