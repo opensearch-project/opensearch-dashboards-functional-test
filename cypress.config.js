9d01ec7c4fef2b5f01aa16b53e654da13572772b
Craig Perkins <cwperx@amazon.com>
2023-08-21 13:43:35 -0400

    Add specPattern
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- cypress.config.js
+++ cypress.config.js
@@ -33,6 +33,7 @@ module.exports = defineConfig({
       return require('./cypress/plugins/index.js')(on, config);
     },
     baseUrl: 'http://localhost:5601',
+    specPattern: '**/*.(js|ts|tsx)',
     testIsolation: false,
   },
 });
