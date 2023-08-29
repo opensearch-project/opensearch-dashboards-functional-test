ddc1cd8dbc1033324d341aed977e47ef1231f944
Kavitha Conjeevaram Mohan <mohakavi@amazon.com>
2023-08-29 14:43:20 -0700

    Fix lint
    Signed-off-by: Kavitha Conjeevaram Mohan <mohakavi@amazon.com>

--- cypress/integration/plugins/reports-dashboards/04-download.spec.js
+++ cypress/integration/plugins/reports-dashboards/04-download.spec.js
@@ -40,7 +40,9 @@ describe('Cypress', () => {
     cy.get('[id="landingPageOnDemandDownload"]')
       .contains('PDF')
       .click({ force: true });
-    cy.get('.euiToastHeader__title').contains('Successfully generated report').should('exist');
+    cy.get('.euiToastHeader__title')
+      .contains('Successfully generated report')
+      .should('exist');
   });
 
   it('Download pdf from in-context menu', () => {
@@ -131,6 +133,8 @@ describe('Cypress', () => {
 
     cy.get('#generateReportFromDetailsFileFormat').click({ force: true });
 
-    cy.get('.euiToastHeader__title').contains('Successfully generated report').should('exist');
+    cy.get('.euiToastHeader__title')
+      .contains('Successfully generated report')
+      .should('exist');
   });
 });
