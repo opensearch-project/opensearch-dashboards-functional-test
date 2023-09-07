6d83e6f50867d3a6ca7a53fca99b7f51514234f2
leanne.laceybyrne@eliatra.com <leanne.laceybyrne@eliatra.com>
2023-09-07 15:29:13 +0100

    remove extra comments
    Signed-off-by: leanneeliatra <leanne.laceybyrne@eliatra.com>
    Signed-off-by: leanne.laceybyrne@eliatra.com <leanne.laceybyrne@eliatra.com>

--- cypress/integration/plugins/security-dashboards-plugin/missing_role_button_logout.js
+++ cypress/integration/plugins/security-dashboards-plugin/missing_role_button_logout.js
@@ -5,19 +5,15 @@
 
 if(Cypress.env('SECURITY_ENABLED')){
     it('Tests if no role button redirects to logout screen', () => {
-        // 1. visit missing roles page
         cy.visit('/app/customerror/missing-role');
 
-        // 2. click the missing role button
         cy.contains('button', 'Back to OpenSearch Dashboards Home').should('be.visible').click();
 
-        // 3. clicking buttong should logout
         cy.intercept({
             method: 'POST',
             url: '/auth/logout',
         });
 
-        // 4. check we have logged out and therefore are back at login page
         cy.getElementByTestId('submit').should('contain.text', 'Log in');
     });
 }
