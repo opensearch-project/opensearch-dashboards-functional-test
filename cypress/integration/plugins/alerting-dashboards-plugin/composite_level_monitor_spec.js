77b84ee966d8e68c33150f95cd7b2ff88756df97
Amardeepsingh Siglani <amardeep7194@gmail.com>
2023-09-19 09:30:47 -0700

    wait before checking update status
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>

--- cypress/integration/plugins/alerting-dashboards-plugin/composite_level_monitor_spec.js
+++ cypress/integration/plugins/alerting-dashboards-plugin/composite_level_monitor_spec.js
@@ -185,9 +185,8 @@ describe('CompositeLevelMonitor', () => {
 
       // Wait for monitor to be created
       cy.wait('@updateMonitorRequest').then(() => {
-        cy.get('.euiTitle--large').contains(
-          `${SAMPLE_VISUAL_EDITOR_MONITOR}_edited`
-        );
+        cy.wait(5000);
+        cy.contains(`${SAMPLE_VISUAL_EDITOR_MONITOR}_edited`);
       });
     });
   });
