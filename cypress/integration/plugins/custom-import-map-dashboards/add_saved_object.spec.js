1e316422574f16fd259e5d4ffa3d6df8f856dfd2
Junqiu Lei <junqiu@amazon.com>
2023-09-19 18:32:33 -0500

    Ignore the flaky test on customImportMaps when security enabled (#881)
    Signed-off-by: Junqiu Lei <junqiu@amazon.com>

--- cypress/integration/plugins/custom-import-map-dashboards/add_saved_object.spec.js
+++ cypress/integration/plugins/custom-import-map-dashboards/add_saved_object.spec.js
@@ -5,33 +5,37 @@
 
 import { BASE_PATH } from '../../../utils/constants';
 
-describe('Add flights dataset saved object', () => {
-  before(() => {
-    cy.visit(`${BASE_PATH}/app/home#/tutorial_directory/sampleData`, {
-      retryOnStatusCodeFailure: true,
-      timeout: 60000,
+if (!Cypress.env('SECURITY_ENABLED')) {
+  describe('Add flights dataset saved object', () => {
+    before(() => {
+      cy.visit(`${BASE_PATH}/app/home#/tutorial_directory/sampleData`, {
+        retryOnStatusCodeFailure: true,
+        timeout: 60000,
+      });
+      cy.get('div[data-test-subj="sampleDataSetCardflights"]', {
+        timeout: 60000,
+      })
+        .contains(/Add data/)
+        .click();
+      cy.wait(60000);
     });
-    cy.get('div[data-test-subj="sampleDataSetCardflights"]', { timeout: 60000 })
-      .contains(/Add data/)
-      .click();
-    cy.wait(60000);
-  });
 
-  after(() => {
-    cy.visit(`${BASE_PATH}/app/home#/tutorial_directory`);
-    cy.get('button[data-test-subj="removeSampleDataSetflights"]')
-      .should('be.visible')
-      .click();
-  });
+    after(() => {
+      cy.visit(`${BASE_PATH}/app/home#/tutorial_directory`);
+      cy.get('button[data-test-subj="removeSampleDataSetflights"]')
+        .should('be.visible')
+        .click();
+    });
 
-  it('check if maps saved object of flights dataset can be found and open', () => {
-    cy.visit(`${BASE_PATH}/app/maps-dashboards`);
-    cy.contains(
-      '[Flights] Flights Status on Maps Destination Location'
-    ).click();
-    cy.get('[data-test-subj="layerControlPanel"]').should(
-      'contain',
-      'Flights On Time'
-    );
+    it('check if maps saved object of flights dataset can be found and open', () => {
+      cy.visit(`${BASE_PATH}/app/maps-dashboards`);
+      cy.contains(
+        '[Flights] Flights Status on Maps Destination Location'
+      ).click();
+      cy.get('[data-test-subj="layerControlPanel"]').should(
+        'contain',
+        'Flights On Time'
+      );
+    });
   });
-});
+}
