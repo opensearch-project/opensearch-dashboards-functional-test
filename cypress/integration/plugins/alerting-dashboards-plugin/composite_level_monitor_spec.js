cdc6e554424f6691536cf631345df2d87cc73566
Amardeepsingh Siglani <amardeep7194@gmail.com>
2023-10-04 12:13:16 -0700

    [Alerting 2.x] Wait before checking update status for composite monitor (#878)
    * wait before checking update status
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * Added timeout to contains check
    
    Co-authored-by: Yulong Ruan <ruanyu1@gmail.com>
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * Removed explicit wait
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * fixed lint error
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    ---------
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    Co-authored-by: Yulong Ruan <ruanyu1@gmail.com>

--- cypress/integration/plugins/alerting-dashboards-plugin/composite_level_monitor_spec.js
+++ cypress/integration/plugins/alerting-dashboards-plugin/composite_level_monitor_spec.js
@@ -185,9 +185,9 @@ describe('CompositeLevelMonitor', () => {
 
       // Wait for monitor to be created
       cy.wait('@updateMonitorRequest').then(() => {
-        cy.get('.euiTitle--large').contains(
-          `${SAMPLE_VISUAL_EDITOR_MONITOR}_edited`
-        );
+        cy.contains(`${SAMPLE_VISUAL_EDITOR_MONITOR}_edited`, {
+          timeout: 5000,
+        });
       });
     });
   });
