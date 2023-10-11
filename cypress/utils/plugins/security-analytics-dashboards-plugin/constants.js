85c7195aef4ba6ee55a91b025bf614198966886d
Amardeepsingh Siglani <amardeep7194@gmail.com>
2023-10-11 12:25:51 -0700

    Security analytics latest tests (#825)
    * updated tests
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * excluded a couple tests; fixed alerts tests
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * dummy change to trigger tests
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * Updated cypress commands to avoid duplication (#835)
    
    * updated commands to avoid duplication
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * updated command header for create rule; changed url for searching rules
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * fix linter issues
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    ---------
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * updated mappings for test index (#841)
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * avoid clicking filter menu btn twice (#849)
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * removed check for url since it differs with and without security (#863)
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * namespaced all commands; updated tests for 2.11
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    * addressed PR comments
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    
    ---------
    
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>

--- cypress/utils/plugins/security-analytics-dashboards-plugin/constants.js
+++ cypress/utils/plugins/security-analytics-dashboards-plugin/constants.js
@@ -3,27 +3,31 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { BASE_PATH } from '../../base_constants';
-
-export const PLUGIN_NAME = 'opensearch_security_analytics_dashboards';
-export const BASE_API_PATH = '/_plugins/_security_analytics';
+import sample_detector from '../../../fixtures/plugins/security-analytics-dashboards-plugin/integration_tests/detector/create_usb_detector_data.json';
 
 export const TWENTY_SECONDS_TIMEOUT = { timeout: 20000 };
+
 export const DETECTOR_TRIGGER_TIMEOUT = 65000;
 
 export const FEATURE_SYSTEM_INDICES = {
   DETECTORS_INDEX: '.opensearch-detectors-config',
   DETECTOR_QUERIES_INDEX: '.opensearch-sap-windows-detectors-queries',
   PRE_PACKAGED_RULES_INDEX: '.opensearch-pre-packaged-rules-config',
-  CUSTOM_RULES_INDEX: '.opensearch-sap-custom-rules-config',
+  CUSTOM_RULES_INDEX: '.opensearch-custom-rules-config',
   WINDOWS_ALERTS_INDEX: '.opensearch-sap-windows-alerts*',
   WINDOWS_FINDINGS_INDEX: '.opensearch-sap-windows-findings*',
 };
 
+export const PLUGIN_NAME = 'opensearch_security_analytics_dashboards';
+
+export const BASE_API_PATH = '/_plugins/_security_analytics';
+
 export const NODE_API = {
   DETECTORS_BASE: `${BASE_API_PATH}/detectors`,
+  CORRELATION_BASE: `${BASE_API_PATH}/correlation/rules`,
   SEARCH_DETECTORS: `${BASE_API_PATH}/detectors/_search`,
   INDICES_BASE: `${BASE_API_PATH}/indices`,
+  FINDINGS_BASE: `${BASE_API_PATH}/findings`,
   GET_FINDINGS: `${BASE_API_PATH}/findings/_search`,
   DOCUMENT_IDS_QUERY: `${BASE_API_PATH}/document_ids_query`,
   TIME_RANGE_QUERY: `${BASE_API_PATH}/time_range_query`,
@@ -31,11 +35,94 @@ export const NODE_API = {
   MAPPINGS_VIEW: `${BASE_API_PATH}/mappings/view`,
   GET_ALERTS: `${BASE_API_PATH}/alerts`,
   RULES_BASE: `${BASE_API_PATH}/rules`,
-  RULES_SEARCH: `${BASE_API_PATH}/rules/_search`,
   CHANNELS: `${BASE_API_PATH}/_notifications/channels`,
   PLUGINS: `${BASE_API_PATH}/_notifications/plugins`,
   ACKNOWLEDGE_ALERTS: `${BASE_API_PATH}/detectors/{detector_id}/_acknowledge/alerts`,
+  UPDATE_ALIASES: `${BASE_API_PATH}/update_aliases`,
+  CORRELATIONS: `${BASE_API_PATH}/correlations`,
+  LOGTYPE_BASE: `${BASE_API_PATH}/logtype`,
   INDEX_TEMPLATE_BASE: '/_index_template',
 };
 
-export const OPENSEARCH_DASHBOARDS_URL = `${BASE_PATH}/app/${PLUGIN_NAME}#`;
+export const { baseUrl: OPENSEARCH_DASHBOARDS } = Cypress.config();
+export const OPENSEARCH_DASHBOARDS_URL = `${OPENSEARCH_DASHBOARDS}/app/${PLUGIN_NAME}#`;
+
+export const createDetector = (
+  detectorName,
+  indexName,
+  indexSettings,
+  indexMappings,
+  ruleSettings,
+  indexDoc,
+  indexDocsCount = 1
+) => {
+  Cypress.log({
+    message: `Create new detector ${detectorName}`,
+  });
+  const detectorConfigAlertCondition = `${detectorName} alert condition`;
+  const detectorConfig = {
+    ...sample_detector,
+    name: detectorName,
+    inputs: [
+      {
+        detector_input: {
+          ...sample_detector.inputs[0].detector_input,
+          description: `Description for ${detectorName}`,
+          indices: [indexName],
+        },
+      },
+    ],
+    triggers: [
+      {
+        ...sample_detector.triggers[0],
+        name: detectorConfigAlertCondition,
+      },
+    ],
+  };
+
+  cy.sa_cleanUpTests()
+    // Create test index
+    .then(() => cy.sa_createIndex(indexName, indexSettings))
+
+    // Create field mappings
+    .then(() =>
+      cy.sa_createAliasMappings(
+        indexName,
+        detectorConfig.detector_type,
+        indexMappings,
+        true
+      )
+    )
+    // Create rule
+    .then(() => {
+      cy.sa_createRule(ruleSettings)
+        .then((response) => {
+          detectorConfig.inputs[0].detector_input.custom_rules[0].id =
+            response.body.response._id;
+          detectorConfig.triggers[0].ids.push(response.body.response._id);
+        })
+        // create the detector
+        .then(() => cy.sa_createDetector(detectorConfig));
+    })
+    .then(() => {
+      // Go to the detectors table page
+      cy.visit(`${OPENSEARCH_DASHBOARDS_URL}/detectors`);
+
+      // Filter table to only show the test detector
+      cy.get(`input[type="search"]`).type(`${detectorConfig.name}{enter}`);
+
+      // Confirm detector was created
+      cy.get('tbody > tr').should(($tr) => {
+        expect($tr, 'detector name').to.contain(detectorConfig.name);
+      });
+    });
+
+  // Wait for the first run to execute before ingesting data
+  cy.wait(65000);
+  // Ingest documents to the test index
+  for (let i = 0; i < indexDocsCount; i++) {
+    cy.sa_insertDocumentToIndex(indexName, '', indexDoc);
+  }
+
+  return detectorConfig;
+};
