a0be908272a67dec0d39211b432c9d415eee780c
Paul Sebastian <paul_sebastian@live.com>
2023-10-30 11:57:24 -0700

    removed observability tests that can be run locally (#939) (#944)
    * removed tests that can be run locally
    * remove unused references
    * fixed import style
    
    Signed-off-by: Paul Sebastian <paulstn@amazon.com>
    (cherry picked from commit 76ec82d381103df058507e6f77c88660c93e82d2)

--- cypress/integration/plugins/observability-dashboards/4_panels.spec.js
+++ cypress/integration/plugins/observability-dashboards/4_panels.spec.js
@@ -16,17 +16,6 @@ const moveToPanelHome = () => {
   cy.wait(delay * 3);
 };
 
-const moveToTestPanel = () => {
-  moveToPanelHome();
-  cy.get('.euiTableCellContent')
-    .contains(TEST_PANEL)
-    .trigger('mouseover')
-    .click();
-  cy.wait(delay * 3);
-  cy.get('h1').contains(TEST_PANEL).should('exist');
-  cy.wait(delay);
-};
-
 describe('Testing panels table', () => {
   beforeEach(() => {
     moveToPanelHome();
@@ -67,97 +56,5 @@ describe('Testing panels table', () => {
       .trigger('mouseover')
       .click();
     cy.wait(delay);
-  });
-
-  it('Deletes panels', () => {
-    cy.get('.panel-header-count').contains('(2)');
-    cy.get('.euiCheckbox__input[data-test-subj="checkboxSelectAll"]')
-      .trigger('mouseover')
-      .click();
-    cy.wait(delay);
-    cy.get('.euiButton__text').contains('Actions').trigger('mouseover').click();
-    cy.wait(delay);
-    cy.get('.euiContextMenuItem__text')
-      .contains('Delete')
-      .trigger('mouseover')
-      .click();
-    cy.wait(delay);
-
-    cy.get('button.euiButton--danger').should('be.disabled');
-
-    cy.get('input.euiFieldText[placeholder="delete"]').focus().type('delete', {
-      delay: 50,
-    });
-    cy.get('button.euiButton--danger').should('not.be.disabled');
-    cy.get('.euiButton__text').contains('Delete').trigger('mouseover').click();
-
-    cy.get('.euiTextAlign')
-      .contains('No Observability Dashboards')
-      .should('exist');
-
-    // keep a panel for testing
-    cy.get('.euiButton__text')
-      .contains('Create Dashboard')
-      .trigger('mouseover')
-      .click();
-    cy.wait(delay);
-    cy.get('input.euiFieldText').focus().type(TEST_PANEL, {
-      delay: 50,
-    });
-    cy.get('.euiButton__text')
-      .contains(/^Create$/)
-      .trigger('mouseover')
-      .click();
-    cy.wait(delay * 2);
-  });
-});
-
-describe('Testing a panel', () => {
-  it('Move to test panel', () => {
-    moveToTestPanel();
-  });
-
-  it('Change date filter of the panel', () => {
-    cy.get(
-      '.euiButtonEmpty[data-test-subj="superDatePickerToggleQuickMenuButton"]'
-    ).click({
-      force: true,
-    });
-    cy.get('.euiLink').contains('This year').trigger('mouseover').click();
-    cy.wait(delay * 2);
-    moveToTestPanel();
-    cy.get(
-      '.euiSuperDatePicker__prettyFormat[data-test-subj="superDatePickerShowDatesButton"]'
-    )
-      .contains('This year')
-      .should('exist');
-    cy.wait(delay);
-  });
-});
-
-describe('Clean up all test data', () => {
-  it.skip('Deletes test panel', () => {
-    moveToPanelHome();
-    cy.get('.euiCheckbox__input[data-test-subj="checkboxSelectAll"]')
-      .trigger('mouseover')
-      .click();
-    cy.wait(delay);
-    cy.get('.euiButton__text').contains('Actions').trigger('mouseover').click();
-    cy.wait(delay);
-    cy.get('.euiContextMenuItem__text')
-      .contains('Delete')
-      .trigger('mouseover')
-      .click();
-    cy.wait(delay);
-    cy.get('button.euiButton--danger').should('be.disabled');
-    cy.get('input.euiFieldText[placeholder="delete"]').focus().type('delete', {
-      delay: 50,
-    });
-    cy.get('button.euiButton--danger').should('not.be.disabled');
-    cy.get('.euiButton__text').contains('Delete').trigger('mouseover').click();
-    cy.wait(delay * 5);
-    cy.get('[data-test-subj="customPanels__noPanelsHome"]')
-      .contains('No Observability Dashboards')
-      .should('exist');
   });
 });
