4772dcf12ce4cabcf4e967bead5ef9f4c32c801a
gaobinlong <gbinlong@amazon.com>
2024-02-06 11:20:01 +0800

    Try again
    Signed-off-by: gaobinlong <gbinlong@amazon.com>

--- cypress/integration/common/dashboard_sample_data_spec.js
+++ cypress/integration/common/dashboard_sample_data_spec.js
@@ -294,26 +294,26 @@ export function dashboardSanityTests() {
           cy.getElementByTestId('copyShareUrlButton').focus().realClick();
 
           // Capture the copied content
-          // cy.window().then((win) => {
-          //   // Access the clipboard contents
-          //   cy.document().then(() => {
-          //     cy.wait(1000); // Wait for clipboard data to be available
-          //     cy.log('Trying to read clipboard data...');
+          cy.window().then((win) => {
+            // Access the clipboard contents
+            cy.document().then(() => {
+              cy.wait(1000); // Wait for clipboard data to be available
+              cy.log('Trying to read clipboard data...');
 
-          //     // Read the clipboard text
-          //     cy.wrap(win.navigator.clipboard.readText()).then((clipboardData) => {
-          //       cy.log('url copied:', clipboardData);
+              // Read the clipboard text
+              cy.wrap(win.navigator.clipboard.readText()).then((clipboardData) => {
+                cy.log('url copied:', clipboardData);
 
-          //       // Assert that the clipboard has data
-          //       expect(clipboardData).to.have.length.greaterThan(0);
+                // Assert that the clipboard has data
+                expect(clipboardData).to.have.length.greaterThan(0);
 
-          //       cy.visit(clipboardData);
-          //       cy.waitForLoader();
+                cy.visit(clipboardData);
+                cy.waitForLoader();
 
-          //       // Now on copied URL page
-          //     });
-          //   });
-          // });
+                // Now on copied URL page
+              });
+            });
+          });
         });
 
       });
