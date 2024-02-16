c2d562182bbe76949f5ea15b925071d59c3df47a
Shenoy Pratik <sgguruda@amazon.com>
2024-02-16 04:57:35 -0800

    revert create button change
    Signed-off-by: Shenoy Pratik <sgguruda@amazon.com>

--- cypress/integration/plugins/gantt-chart-dashboards/gantt_ui.spec.js
+++ cypress/integration/plugins/gantt-chart-dashboards/gantt_ui.spec.js
@@ -61,7 +61,7 @@ describe('Save a gantt chart', () => {
   });
 
   it('Creates and saves a gantt chart', () => {
-    cy.contains('Create ').click({ force: true });
+    cy.get('.euiButton__text').contains('Create ').click({ force: true });
     cy.wait(delay);
     cy.get('[data-test-subj="visTypeTitle"]')
       .contains('Gantt Chart')
