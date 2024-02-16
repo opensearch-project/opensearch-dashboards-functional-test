f8487b88bfa64d5c6331f9b383aaff59f16bdb4a
Simeon Widdis <sawiddis@amazon.com>
2024-02-15 16:34:50 -0800

    Fix in-context report tests
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- cypress/integration/plugins/observability-dashboards/6_notebooks.spec.js
+++ cypress/integration/plugins/observability-dashboards/6_notebooks.spec.js
@@ -115,7 +115,7 @@ describe('Test reporting integration if plugin installed', () => {
     cy.get('button.euiContextMenuItem:nth-child(1)')
       .contains('Download PDF')
       .click();
-    cy.get('#downloadInProgressLoadingModal').should('exist');
+    cy.get('#downloadInProgressLoadingModal').should('not.exist');
   });
 
   it('Create in-context PNG report from notebook', () => {
@@ -123,7 +123,7 @@ describe('Test reporting integration if plugin installed', () => {
     cy.get('button.euiContextMenuItem:nth-child(2)')
       .contains('Download PNG')
       .click();
-    cy.get('#downloadInProgressLoadingModal').should('exist');
+    cy.get('#downloadInProgressLoadingModal').should('not.exist');
   });
 
   it('Create on-demand report definition from context menu', () => {
