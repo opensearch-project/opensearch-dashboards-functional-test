001a20f5eca31d58453956fa8b0cff15f3a2b57c
Amardeepsingh Siglani <amardeep7194@gmail.com>
2024-02-17 04:05:39 +0530

    [Security analytics] add force option to avoid element getting covered error (#1102)
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>
    (cherry picked from commit eb10f07eb247aac71d08f4d96920be88b4125ddf)

--- cypress/integration/plugins/security-analytics-dashboards-plugin/2_rules.spec.js
+++ cypress/integration/plugins/security-analytics-dashboards-plugin/2_rules.spec.js
@@ -513,7 +513,7 @@ describe('Rules', () => {
       toastShouldExist();
 
       getSelectionPanelByIndex(0).within(() =>
-        getMapValueField().type('FieldValue')
+        getMapValueField().type('FieldValue', { force: true })
       );
 
       // selection map list field
@@ -524,7 +524,7 @@ describe('Rules', () => {
       toastShouldExist();
       getSelectionPanelByIndex(0).within(() => {
         getListRadioField().click({ force: true });
-        getMapListField().type('FieldValue');
+        getMapListField().type('FieldValue', { force: true });
       });
 
       // tags field
