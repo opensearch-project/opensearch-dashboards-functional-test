b70706915de53cb3a698e7558dc8379dd993729c
Lin Wang <wonglam@amazon.com>
2024-03-18 17:42:48 +0800

    Change to use pre trained model
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- cypress/integration/plugins/ml-commons-dashboards/overview_spec.js
+++ cypress/integration/plugins/ml-commons-dashboards/overview_spec.js
@@ -4,40 +4,28 @@
  */
 import { MLC_URL, MLC_DASHBOARD_API } from '../../../utils/constants';
 
+const UPLOAD_MODEL_NAME = 'huggingface/sentence-transformers/all-MiniLM-L6-v2';
+
 if (Cypress.env('ML_COMMONS_DASHBOARDS_ENABLED')) {
   describe('MLC Overview page', () => {
     let uploadedModelId;
     let uploadedModelLoadedError;
-    const uploadModelName = `traced_small_model-${new Date()
-      .getTime()
-      .toString(34)}`;
+
     before(() => {
       // Disable only_run_on_ml_node to avoid model upload error in case of cluster no ml nodes
       cy.disableOnlyRunOnMLNode();
       cy.disableNativeMemoryCircuitBreaker();
-      cy.enableRegisterModelViaURL();
       cy.wait(1000);
 
       cy.registerModelGroup({
-        name: 'model-group',
+        name: `model-group-${new Date().getTime().toString(34)}`,
       })
         .then(({ model_group_id }) =>
-          cy.uploadModelByUrl({
-            name: uploadModelName,
-            version: '1.0.0',
-            model_format: 'TORCH_SCRIPT',
-            model_task_type: 'text_embedding',
+          cy.registerModel({
+            name: UPLOAD_MODEL_NAME,
+            version: '1.0.1',
             model_group_id,
-            model_content_hash_value:
-              'e13b74006290a9d0f58c1376f9629d4ebc05a0f9385f40db837452b167ae9021',
-            model_config: {
-              model_type: 'bert',
-              embedding_dimension: 768,
-              framework_type: 'sentence_transformers',
-              all_config:
-                '{"architectures":["BertModel"],"max_position_embeddings":512,"model_type":"bert","num_attention_heads":12,"num_hidden_layers":6}',
-            },
-            url: 'https://github.com/opensearch-project/ml-commons/blob/2.x/ml-algorithms/src/test/resources/org/opensearch/ml/engine/algorithms/text_embedding/traced_small_model.zip?raw=true',
+            model_format: 'TORCH_SCRIPT',
           })
         )
         .then(({ task_id: taskId }) =>
@@ -80,13 +68,13 @@ if (Cypress.env('ML_COMMONS_DASHBOARDS_ENABLED')) {
       cy.contains('h1', 'Overview');
       cy.contains('h2', 'Deployed models');
 
-      cy.get('[aria-label="Search by name or ID"]').type(uploadModelName);
+      cy.get('[aria-label="Search by name or ID"]').type(uploadedModelId);
 
       cy.contains(uploadedModelId)
         .closest('tr')
         .contains(uploadedModelLoadedError ? 'Not responding' : 'Responding')
         .closest('tr')
-        .contains(uploadModelName);
+        .contains(UPLOAD_MODEL_NAME);
 
       cy.contains('h1', 'Overview');
       cy.contains('h2', 'Deployed models');
@@ -95,14 +83,14 @@ if (Cypress.env('ML_COMMONS_DASHBOARDS_ENABLED')) {
     it('should open preview panel after view detail button click', () => {
       cy.visit(MLC_URL.OVERVIEW);
 
-      cy.get('[aria-label="Search by name or ID"]').type(uploadModelName);
+      cy.get('[aria-label="Search by name or ID"]').type(uploadedModelId);
 
       cy.contains(uploadedModelId)
         .closest('tr')
         .find('[aria-label="view detail"]')
         .click();
 
-      cy.contains('.euiFlyoutHeader > h3', uploadModelName);
+      cy.contains('.euiFlyoutHeader > h3', UPLOAD_MODEL_NAME);
       cy.contains('.euiFlyoutBody', uploadedModelId);
     });
 
@@ -122,7 +110,7 @@ if (Cypress.env('ML_COMMONS_DASHBOARDS_ENABLED')) {
 
       cy.visit(MLC_URL.OVERVIEW);
 
-      cy.get('[aria-label="Search by name or ID"]').type(uploadModelName);
+      cy.get('[aria-label="Search by name or ID"]').type(uploadedModelId);
 
       cy.contains(uploadedModelId)
         .closest('tr')
