ffb5feecd876b38db9a7f57872c6646ee5f8f126
Amardeepsingh Siglani <amardeep7194@gmail.com>
2024-03-19 14:17:16 -0700

    updated tests to match repo
    Signed-off-by: Amardeepsingh Siglani <amardeep7194@gmail.com>

--- cypress/integration/plugins/security-analytics-dashboards-plugin/4_findings.spec.js
+++ cypress/integration/plugins/security-analytics-dashboards-plugin/4_findings.spec.js
@@ -152,5 +152,26 @@ describe('Findings', () => {
     });
   });
 
+  it('shows document not found warning when the document is empty', () => {
+    cy.deleteIndex(indexName);
+    cy.reload();
+
+    // Wait for page to load
+    cy.waitForPageLoad('findings', {
+      contains: 'Findings',
+    });
+
+    // filter table to show only sample_detector findings
+    cy.get(`input[placeholder="Search findings"]`).ospSearch(indexName);
+
+    // open Finding details flyout via finding id link. cy.wait essential, timeout insufficient.
+    cy.getTableFirstRow('[data-test-subj="view-details-icon"]').then(($el) => {
+      cy.get($el).click({ force: true });
+    });
+
+    // Flyout should show 'Document not found' warning
+    cy.contains('Document not found');
+  });
+
   after(() => cy.sa_cleanUpTests());
 });
