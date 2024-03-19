424f59c73b745cca762fa11a43abbc8dc460d562
Junqiu Lei <junqiu@amazon.com>
2024-03-19 01:28:42 -0700

    update data source name and rebase code in maps (#1147)
    Signed-off-by: Junqiu Lei <junqiu@amazon.com>

--- cypress/integration/plugins/custom-import-map-dashboards/3_add_saved_object.spec.js
+++ cypress/integration/plugins/custom-import-map-dashboards/3_add_saved_object.spec.js
@@ -4,14 +4,19 @@
  */
 
 import { BASE_PATH } from '../../../utils/constants';
+import { CURRENT_TENANT } from '../../../utils/commands';
 
 describe('Add flights dataset saved object', () => {
   before(() => {
+    CURRENT_TENANT.newTenant = 'global';
     cy.visit(`${BASE_PATH}/app/home#/tutorial_directory/sampleData`, {
       retryOnStatusCodeFailure: true,
       timeout: 60000,
     });
-    cy.get('div[data-test-subj="sampleDataSetCardflights"]', { timeout: 60000 })
+    cy.wait(5000);
+    cy.get('div[data-test-subj="sampleDataSetCardflights"]', {
+      timeout: 60000,
+    })
       .contains(/Add data/)
       .click();
     cy.wait(60000);
@@ -19,19 +24,25 @@ describe('Add flights dataset saved object', () => {
 
   after(() => {
     cy.visit(`${BASE_PATH}/app/home#/tutorial_directory`);
-    cy.get('button[data-test-subj="removeSampleDataSetflights"]')
+    cy.wait(5000);
+    cy.get('button[data-test-subj="removeSampleDataSetflights"]', {
+      timeout: 120000,
+    })
       .should('be.visible')
       .click();
   });
 
   it('check if maps saved object of flights dataset can be found and open', () => {
+    cy.wait(10000);
     cy.visit(`${BASE_PATH}/app/maps-dashboards`);
-    cy.contains(
-      '[Flights] Flights Status on Maps Destination Location'
-    ).click();
+    cy.wait(10000);
+    cy.contains('[Flights] Flights Status on Maps Destination Location', {
+      timeout: 120000,
+    }).click();
     cy.get('[data-test-subj="layerControlPanel"]').should(
       'contain',
-      'Flights On Time'
+      'Flights On Time',
+      { timeout: 120000 }
     );
   });
 });
