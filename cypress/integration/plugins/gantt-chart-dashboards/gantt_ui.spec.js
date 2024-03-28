b987d1333bc2485b1b97ebb0f5515b86a7d22acb
Sean Li <lnse@amazon.com>
2024-03-28 04:24:49 -0700

    try global tenant
    Signed-off-by: Sean Li <lnse@amazon.com>

--- cypress/integration/plugins/gantt-chart-dashboards/gantt_ui.spec.js
+++ cypress/integration/plugins/gantt-chart-dashboards/gantt_ui.spec.js
@@ -57,7 +57,7 @@ describe('Dump test data', () => {
 
 describe('Save a gantt chart', { defaultCommandTimeout: 20000 }, () => {
   beforeEach(() => {
-    CURRENT_TENANT.newTenant = 'private';
+    CURRENT_TENANT.newTenant = 'global';
     cy.visit(`${BASE_PATH}/app/visualize#`);
   });
 
@@ -82,7 +82,7 @@ describe(
   { defaultCommandTimeout: 20000 },
   () => {
     beforeEach(() => {
-      CURRENT_TENANT.newTenant = 'private';
+      CURRENT_TENANT.newTenant = 'global';
       cy.visit(`${BASE_PATH}/app/visualize#`);
       cy.get('.euiFieldSearch').focus().type(GANTT_VIS_NAME);
       cy.contains(GANTT_VIS_NAME).click({ force: true });
@@ -122,7 +122,7 @@ describe(
 
 describe('Configure panel settings', { defaultCommandTimeout: 20000 }, () => {
   beforeEach(() => {
-    CURRENT_TENANT.newTenant = 'private';
+    CURRENT_TENANT.newTenant = 'global';
     cy.visit(`${BASE_PATH}/app/visualize#`);
     cy.get('.euiFieldSearch').focus().type(GANTT_VIS_NAME);
     cy.contains(GANTT_VIS_NAME).click({ force: true });
