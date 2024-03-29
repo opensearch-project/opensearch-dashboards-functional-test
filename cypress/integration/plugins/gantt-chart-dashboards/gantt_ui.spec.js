f7aa109f0bbc7266ac26025f6dfb1e2236f0674f
Sean Li <lnse@amazon.com>
2024-03-28 20:06:28 -0700

    adding wait for request
    Signed-off-by: Sean Li <lnse@amazon.com>

--- cypress/integration/plugins/gantt-chart-dashboards/gantt_ui.spec.js
+++ cypress/integration/plugins/gantt-chart-dashboards/gantt_ui.spec.js
@@ -126,7 +126,9 @@ describe(
 describe('Configure panel settings', { defaultCommandTimeout: 20000 }, () => {
   beforeEach(() => {
     cy.visit(`${BASE_PATH}/app/visualize#`);
+    cy.intercept('**').as('searchRequest');
     cy.get('.euiFieldSearch').focus().type(GANTT_VIS_NAME);
+    cy.wait('@searchRequest');
     cy.contains(GANTT_VIS_NAME).should('exist').click({ force: true });
     cy.contains('Panel settings').click({ force: true });
   });
