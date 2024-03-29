887b5b87d558e1c6af55b54f1c6e21b996e2dfce
Sean Li <lnse@amazon.com>
2024-03-28 18:30:57 -0700

    switch to private tenant
    Signed-off-by: Sean Li <lnse@amazon.com>

--- cypress/integration/plugins/gantt-chart-dashboards/gantt_ui.spec.js
+++ cypress/integration/plugins/gantt-chart-dashboards/gantt_ui.spec.js
@@ -20,7 +20,7 @@ const DEFAULT_SIZE = 10;
 
 describe('Dump test data', () => {
   it('Indexes test data for gantt chart', () => {
-    CURRENT_TENANT.newTenant = 'global';
+    CURRENT_TENANT.newTenant = 'private';
     const dumpDataSet = (ndjson, index) =>
       cy.request({
         method: 'POST',
@@ -56,10 +56,19 @@ describe('Dump test data', () => {
 });
 
 describe('Save a gantt chart', { defaultCommandTimeout: 20000 }, () => {
-  before(() => {
-    CURRENT_TENANT.newTenant = 'global';
-  });
   beforeEach(() => {
+    if (Cypress.env('SECURITY_ENABLED')) {
+      /**
+       * Security plugin is using private tenant as default.
+       * So here we'd need to set global tenant as default manually.
+       */
+      cy.changeDefaultTenant({
+        multitenancy_enabled: true,
+        private_tenant_enabled: true,
+        default_tenant: 'private',
+      });
+    }
+    CURRENT_TENANT.newTenant = 'private';
     cy.visit(`${BASE_PATH}/app/visualize#`);
   });
 
@@ -83,10 +92,19 @@ describe(
   'Render and configure a gantt chart',
   { defaultCommandTimeout: 20000 },
   () => {
-    before(() => {
-      CURRENT_TENANT.newTenant = 'global';
-    });
     beforeEach(() => {
+      if (Cypress.env('SECURITY_ENABLED')) {
+        /**
+         * Security plugin is using private tenant as default.
+         * So here we'd need to set global tenant as default manually.
+         */
+        cy.changeDefaultTenant({
+          multitenancy_enabled: true,
+          private_tenant_enabled: true,
+          default_tenant: 'private',
+        });
+      }
+      CURRENT_TENANT.newTenant = 'private';
       cy.visit(`${BASE_PATH}/app/visualize#`);
       cy.get('.euiFieldSearch').focus().type(GANTT_VIS_NAME);
       cy.contains(GANTT_VIS_NAME).click({ force: true });
@@ -125,10 +143,19 @@ describe(
 );
 
 describe('Configure panel settings', { defaultCommandTimeout: 20000 }, () => {
-  before(() => {
-    CURRENT_TENANT.newTenant = 'global';
-  });
   beforeEach(() => {
+    if (Cypress.env('SECURITY_ENABLED')) {
+      /**
+       * Security plugin is using private tenant as default.
+       * So here we'd need to set global tenant as default manually.
+       */
+      cy.changeDefaultTenant({
+        multitenancy_enabled: true,
+        private_tenant_enabled: true,
+        default_tenant: 'private',
+      });
+    }
+    CURRENT_TENANT.newTenant = 'private';
     cy.visit(`${BASE_PATH}/app/visualize#`);
     cy.get('.euiFieldSearch').focus().type(GANTT_VIS_NAME);
     cy.contains(GANTT_VIS_NAME).click({ force: true });
@@ -251,9 +278,6 @@ describe(
   'Add gantt chart to dashboard',
   { defaultCommandTimeout: 20000 },
   () => {
-    before(() => {
-      CURRENT_TENANT.newTenant = 'global';
-    });
     it('Adds gantt chart to dashboard', () => {
       cy.visit(`${BASE_PATH}/app/dashboards#/create`);
       cy.contains('Add an existing').click({ force: true });
