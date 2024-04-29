7ab26e2c9423ae7791f9cf537ebc81594b4b36c0
Yulong Ruan <ruanyl@amazon.com>
2024-04-29 17:01:05 +0800

    [Workspace] add tests for workspace dropdown menu (#1228)
    * add tests for workspace dropdown menu
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
    
    * do not show welcome page
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
    
    * fix failed test
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
    
    ---------
    
    Signed-off-by: Yulong Ruan <ruanyl@amazon.com>
    (cherry picked from commit f9ef54b21dd9f1a845bcf64c48cdd39db95d9af4)

--- cypress/utils/dashboards/workspace-plugin/commands.js
+++ cypress/utils/dashboards/workspace-plugin/commands.js
@@ -164,3 +164,26 @@ function checkPrincipalArrayEquals(expectedPrincipals, actualPrincipals) {
   });
   return true;
 }
+
+Cypress.Commands.add('deleteAllWorkspaces', () => {
+  cy.request({
+    method: 'POST',
+    url: `${BASE_PATH}${WORKSPACE_API_PREFIX}/_list`,
+    headers: {
+      'osd-xsrf': true,
+    },
+    body: {},
+  }).then((resp) => {
+    if (resp && resp.body && resp.body.success) {
+      resp.body.result.workspaces.forEach(({ id }) => {
+        cy.request({
+          method: 'DELETE',
+          url: `${BASE_PATH}${WORKSPACE_API_PREFIX}/${id}`,
+          headers: {
+            'osd-xsrf': true,
+          },
+        });
+      });
+    }
+  });
+});
