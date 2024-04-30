1b19f50aeb58d9479fdf4bbaa07e271e12198b69
Hailong Cui <ihailong@amazon.com>
2024-04-30 15:42:46 +0800

    [Workspace] Add functional test for workspace overview page (#1246)
    * workspace overview
    
    Signed-off-by: Hailong Cui <ihailong@amazon.com>
    
    * update create workspace
    
    Signed-off-by: Hailong Cui <ihailong@amazon.com>
    
    * address review comments
    
    Signed-off-by: Hailong Cui <ihailong@amazon.com>
    
    * Add assertion
    
    Signed-off-by: Hailong Cui <ihailong@amazon.com>
    
    * fix workspacd name
    
    Signed-off-by: Hailong Cui <ihailong@amazon.com>
    
    ---------
    
    Signed-off-by: Hailong Cui <ihailong@amazon.com>

--- cypress/integration/core-opensearch-dashboards/opensearch-dashboards/workspace-plugin/workspace_dropdown_menu.spec.js
+++ cypress/integration/core-opensearch-dashboards/opensearch-dashboards/workspace-plugin/workspace_dropdown_menu.spec.js
@@ -20,7 +20,7 @@ if (Cypress.env('WORKSPACE_ENABLED')) {
       cy.deleteAllWorkspaces();
 
       for (const workspaceName of workspaceNames) {
-        cy.createWorkspace(workspaceName).then((workspaceId) => {
+        cy.createWorkspace({ name: workspaceName }).then((workspaceId) => {
           workspaceNameIdMapping[workspaceName] = workspaceId;
         });
       }
@@ -98,10 +98,11 @@ if (Cypress.env('WORKSPACE_ENABLED')) {
 
       // Create 3 more workspaces so that there are more than 5 workspaces in the system
       for (const workspaceName of newWorkspaceNames) {
-        cy.createWorkspace(workspaceName);
+        cy.createWorkspace({ name: workspaceName });
       }
 
       cy.wait(1000);
+      cy.reload();
 
       cy.getElementByTestId('toggleNavButton').click();
       cy.get('#workspaceDropdownMenu').click();
