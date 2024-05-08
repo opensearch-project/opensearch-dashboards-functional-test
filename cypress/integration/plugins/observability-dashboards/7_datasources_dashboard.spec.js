0f4b038e9a22aca15908453790e0e6509c4c31fc
Ryan Liang <jiallian@amazon.com>
2024-05-08 14:35:18 -0700

    Skip datasources tests for managed service run
    Signed-off-by: Ryan Liang <jiallian@amazon.com>

--- cypress/integration/plugins/observability-dashboards/7_datasources_dashboard.spec.js
+++ cypress/integration/plugins/observability-dashboards/7_datasources_dashboard.spec.js
@@ -25,52 +25,54 @@ const visitDatasourcesCreationPage = () => {
   cy.visit(BASE_PATH + DATASOURCES_PATH.DATASOURCES_CREATION_BASE);
 };
 
-describe('Integration tests for datasources plugin', () => {
-  it('Navigates to datasources plugin and expects the correct header', () => {
-    visitDatasourcesHomePage();
-    cy.get('[data-test-subj="dataconnections-header"]', {
-      timeout: 120000,
-    }).should('exist');
+if (!Cypress.env('MANAGED_SERVICE_ENDPOINT')) {
+  describe('Integration tests for datasources plugin', () => {
+    it('Navigates to datasources plugin and expects the correct header', () => {
+      visitDatasourcesHomePage();
+      cy.get('[data-test-subj="dataconnections-header"]', {
+        timeout: 120000,
+      }).should('exist');
+    });
+
+    it('Tests navigation between tabs', () => {
+      visitDatasourcesHomePage();
+
+      cy.get(manageDataSourcesTag)
+        .should('have.class', 'euiTab-isSelected')
+        .and('have.attr', 'aria-selected', 'true');
+      cy.get(manageDataSourcesTag).click();
+      cy.url().should('include', '/manage');
+
+      cy.get(newDataSourcesTag).click();
+      cy.get(newDataSourcesTag)
+        .should('have.class', 'euiTab-isSelected')
+        .and('have.attr', 'aria-selected', 'true');
+      cy.url().should('include', '/new');
+
+      cy.get(createS3Button).should('be.visible');
+      cy.get(createPrometheusButton).should('be.visible');
+    });
+
+    it('Tests navigation of S3 datasources creation page with hash', () => {
+      visitDatasourcesCreationPage();
+
+      cy.get(createS3Button).should('be.visible').click();
+      cy.url().should('include', 'configure/AmazonS3AWSGlue');
+
+      cy.get('h1.euiTitle.euiTitle--medium')
+        .should('be.visible')
+        .and('contain', 'Configure Amazon S3 data source');
+    });
+
+    it('Tests navigation of Prometheus datasources creation page with hash', () => {
+      visitDatasourcesCreationPage();
+
+      cy.get(createPrometheusButton).should('be.visible').click();
+      cy.url().should('include', 'configure/Prometheus');
+
+      cy.get('h4.euiTitle.euiTitle--medium')
+        .should('be.visible')
+        .and('contain', 'Configure Prometheus data source');
+    });
   });
-
-  it('Tests navigation between tabs', () => {
-    visitDatasourcesHomePage();
-
-    cy.get(manageDataSourcesTag)
-      .should('have.class', 'euiTab-isSelected')
-      .and('have.attr', 'aria-selected', 'true');
-    cy.get(manageDataSourcesTag).click();
-    cy.url().should('include', '/manage');
-
-    cy.get(newDataSourcesTag).click();
-    cy.get(newDataSourcesTag)
-      .should('have.class', 'euiTab-isSelected')
-      .and('have.attr', 'aria-selected', 'true');
-    cy.url().should('include', '/new');
-
-    cy.get(createS3Button).should('be.visible');
-    cy.get(createPrometheusButton).should('be.visible');
-  });
-
-  it('Tests navigation of S3 datasources creation page with hash', () => {
-    visitDatasourcesCreationPage();
-
-    cy.get(createS3Button).should('be.visible').click();
-    cy.url().should('include', 'configure/AmazonS3AWSGlue');
-
-    cy.get('h1.euiTitle.euiTitle--medium')
-      .should('be.visible')
-      .and('contain', 'Configure Amazon S3 data source');
-  });
-
-  it('Tests navigation of Prometheus datasources creation page with hash', () => {
-    visitDatasourcesCreationPage();
-
-    cy.get(createPrometheusButton).should('be.visible').click();
-    cy.url().should('include', 'configure/Prometheus');
-
-    cy.get('h4.euiTitle.euiTitle--medium')
-      .should('be.visible')
-      .and('contain', 'Configure Prometheus data source');
-  });
-});
+}
