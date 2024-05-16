5beb70c20aa38e270c167bf9008695886579f956
Craig Perkins <cwperx@amazon.com>
2024-05-16 12:19:14 -0400

    Update all instances
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- cypress/integration/plugins/security/tenants_spec.js
+++ cypress/integration/plugins/security/tenants_spec.js
@@ -69,7 +69,7 @@ if (Cypress.env('SECURITY_ENABLED')) {
         expect(url).to.contain('/tenants');
       });
 
-      cy.contains('h3', 'Tenants');
+      cy.contains('h3', 'Dashboards tenants');
       // should contain the new tenant that was just created
       cy.contains('.euiTableCellContent', tenantName);
       cy.contains('span', tenantDescription);
