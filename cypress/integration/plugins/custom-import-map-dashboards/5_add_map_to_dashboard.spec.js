9547e50cd5a6861388b3eec203381e6129635f80
Junqiu Lei <junqiu@amazon.com>
2024-08-30 16:50:43 -0700

    Add new home ui test for maps (#1542)
    Signed-off-by: Junqiu Lei <junqiu@amazon.com>
    (cherry picked from commit 34ba5be6db8f75e5da5e369b5311d3077f2ea521)

--- cypress/integration/plugins/custom-import-map-dashboards/5_add_map_to_dashboard.spec.js
+++ cypress/integration/plugins/custom-import-map-dashboards/5_add_map_to_dashboard.spec.js
@@ -4,8 +4,8 @@
  */
 
 import { BASE_PATH } from '../../../utils/constants';
-import { MiscUtils } from '@opensearch-dashboards-test/opensearch-dashboards-test-library';
 import { CURRENT_TENANT } from '../../../utils/commands';
+import { MiscUtils } from '@opensearch-dashboards-test/opensearch-dashboards-test-library';
 
 const miscUtils = new MiscUtils(cy);
 
@@ -17,21 +17,24 @@ describe('Add map to dashboard', () => {
     cy.wait(15000);
   });
 
+  after(() => {
+    miscUtils.removeSampleData();
+  });
+
   it('Add new map to dashboard', () => {
     const testMapName = 'saved-map-' + Date.now().toString();
-    cy.visit(`${BASE_PATH}/app/dashboards`);
-    cy.get('[data-test-subj="newItemButton"]').click();
-    cy.get('button[data-test-subj="dashboardAddNewPanelButton"]').click();
-    cy.get('button[data-test-subj="visType-customImportMap"]').click();
+    cy.visit(`${BASE_PATH}/app/dashboards#/create`);
+    cy.wait(5000)
+      .get('button[data-test-subj="addVisualizationButton"]')
+      .click();
+    cy.wait(5000)
+      .get('button[data-test-subj="visType-customImportMap"]')
+      .click();
     cy.wait(5000).get('button[data-test-subj="mapSaveButton"]').click();
     cy.wait(5000).get('[data-test-subj="savedObjectTitle"]').type(testMapName);
     cy.wait(5000)
       .get('[data-test-subj="confirmSaveSavedObjectButton"]')
       .click();
-    cy.get('.embPanel__titleText').should('contain', testMapName);
-  });
-
-  after(() => {
-    miscUtils.removeSampleData();
+    cy.wait(5000).get('.embPanel__titleText').should('contain', testMapName);
   });
 });
