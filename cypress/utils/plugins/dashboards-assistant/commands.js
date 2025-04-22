8ae60bbde3478ea9d1f48a5b4d097a4b7b18f6f3
Lin Wang <wonglam@amazon.com>
2025-04-22 14:32:59 +0800

    Refactor chatbot agent registration
    Signed-off-by: Lin Wang <wonglam@amazon.com>

--- cypress/utils/plugins/dashboards-assistant/commands.js
+++ cypress/utils/plugins/dashboards-assistant/commands.js
@@ -4,9 +4,14 @@
  */
 
 import { get } from 'lodash';
-import FlowTemplateJSON from '../../../fixtures/plugins/dashboards-assistant/flow-template.json';
+import chatFlowTemplateJSON from '../../../fixtures/plugins/dashboards-assistant/flow-templates/chat.json';
 import { BACKEND_BASE_PATH, BASE_PATH } from '../../constants';
-import { ML_COMMONS_API, ASSISTANT_API } from './constants';
+import {
+  ML_COMMONS_API,
+  ASSISTANT_API,
+  FLOW_FRAMEWORK_API,
+  ASSISTANT_AGENT_NAME,
+} from './constants';
 import clusterSettings from '../../../fixtures/plugins/dashboards-assistant/cluster_settings.json';
 import { apiRequest } from '../../helpers';
 import {
@@ -18,123 +23,143 @@ Cypress.Commands.add('addAssistantRequiredSettings', () => {
   cy.request('PUT', `${BACKEND_BASE_PATH}/_cluster/settings`, clusterSettings);
 });
 
-const agentParameters = {
-  connectorId: '',
-  modelId: '',
-  conversationalAgentId: '',
-  rootAgentId: '',
-};
+const provisionedWorkflows = [];
+const agents = [
+  {
+    type: 'os_chat_root_agent',
+    agentName: ASSISTANT_AGENT_NAME.CHAT,
+    flowTemplateJSON: chatFlowTemplateJSON,
+  },
+];
 
-Cypress.Commands.add('readOrRegisterRootAgent', () => {
-  cy.request({
-    url: `${BACKEND_BASE_PATH}${ML_COMMONS_API.AGENT_CONFIG}`,
-    method: 'GET',
-    failOnStatusCode: false,
-  }).then((resp) => {
-    const agentId = get(resp, 'body.configuration.agent_id');
-    if (agentId) {
-      cy.log(`Already initialized agent: ${agentId}, skip the initialize step`);
-    } else {
-      cy.log(`Agent id not initialized yet, set up agent`);
-      return cy.registerRootAgent();
-    }
+Cypress.Commands.add('prepareAssistantAgents', () => {
+  agents.forEach((agent) => {
+    cy.readOrRegisterRootAgent(agent);
   });
 });
 
-Cypress.Commands.add('registerRootAgent', () => {
-  /**
-   * TODO use flow framework if the plugin get integrate in the future.
-   */
-  // cy.request(
-  //   'POST',
-  //   `${BACKEND_BASE_PATH}${FLOW_FRAMEWORK_API.CREATE_FLOW_TEMPLATE}`,
-  //   FlowTemplateJSON
-  // ).then((resp) => {
-  //   usingWorkflowId = resp.body.workflow_id;
-  //   if (usingWorkflowId) {
-  //     cy.request(
-  //       'POST',
-  //       `${BACKEND_BASE_PATH}${FLOW_FRAMEWORK_API.CREATE_FLOW_TEMPLATE}/${usingWorkflowId}/_provision`
-  //     );
-  //     /**
-  //      * wait for 2s
-  //      */
-  //     cy.wait(2000);
-  //   } else {
-  //     throw new Error(resp);
-  //   }
-  // });
-  const nodesMap = {};
-  FlowTemplateJSON.workflows.provision.nodes.forEach((node) => {
-    nodesMap[node.type] = nodesMap[node.type] || [];
-    nodesMap[node.type].push(node);
-  });
-  cy.request(
-    'POST',
-    `${BACKEND_BASE_PATH}${ML_COMMONS_API.CREATE_CONNECTOR}`,
-    nodesMap.create_connector[0].user_inputs
-  )
-    .then((resp) => {
-      agentParameters.connectorId = resp.body.connector_id;
-      return cy.request(
-        'POST',
-        `${BACKEND_BASE_PATH}${ML_COMMONS_API.CREATE_MODEL}?deploy=true`,
-        {
-          ...nodesMap.register_remote_model[0].user_inputs,
-          connector_id: agentParameters.connectorId,
-          function_name: 'remote',
+Cypress.Commands.add(
+  'readOrRegisterRootAgent',
+  ({ type, agentName, flowTemplateJSON }) =>
+    cy
+      .request({
+        url: `${BACKEND_BASE_PATH}${ML_COMMONS_API.AGENT_CONFIG.replace(
+          '<agent_name>',
+          agentName
+        )}`,
+        method: 'GET',
+        failOnStatusCode: false,
+      })
+      .then((resp) => {
+        const agentId = get(resp, 'body.configuration.agent_id');
+        if (agentId) {
+          cy.log(
+            `Already initialized agent: ${agentId}, skip the initialize step`
+          );
+        } else {
+          cy.log(`Agent id not initialized yet, set up agent`);
+          return cy.registerAgent({
+            flowTemplateJSON,
+            agentName,
+            type,
+          });
         }
-      );
-    })
-    .then((resp) => {
-      agentParameters.modelId = resp.body.model_id;
-      return cy.request(
-        'POST',
-        `${BACKEND_BASE_PATH}${ML_COMMONS_API.CREATE_AGENT}`,
-        {
-          ...nodesMap.register_agent[0].user_inputs,
-          llm: {
-            parameters:
-              nodesMap.register_agent[0].user_inputs['llm.parameters'],
-            model_id: agentParameters.modelId,
-          },
-          tools: [nodesMap.create_tool[0]].map((item) => item.user_inputs),
-        }
-      );
-    })
-    .then((resp) => {
-      agentParameters.conversationalAgentId = resp.body.agent_id;
-      return cy.request(
-        'POST',
-        `${BACKEND_BASE_PATH}${ML_COMMONS_API.CREATE_AGENT}`,
-        {
-          ...nodesMap.register_agent[1].user_inputs,
-          tools: [
-            {
-              ...nodesMap.create_tool[1].user_inputs,
-              parameters: {
-                ...nodesMap.create_tool[1].user_inputs.parameters,
-                agent_id: agentParameters.conversationalAgentId,
-              },
-            },
-            {
-              ...nodesMap.create_tool[2].user_inputs,
-              parameters: {
-                ...nodesMap.create_tool[2].user_inputs.parameters,
-                model_id: agentParameters.modelId,
-              },
-            },
-          ],
-        }
-      );
-    })
-    .then((resp) => {
-      agentParameters.rootAgentId = resp.body.agent_id;
-      return cy.putRootAgentId(agentParameters.rootAgentId);
-    });
-});
+      })
+);
 
-Cypress.Commands.add('putRootAgentId', (agentId) => {
+Cypress.Commands.add(
+  'requestPollUntil',
+  (requestConfig, predicate, options = {}) => {
+    const { timeout = 30000, interval = 1000 } = options;
+    const startTime = Date.now();
+
+    const attempt = () => {
+      // Check if we've exceeded timeout
+      if (Date.now() - startTime > timeout) {
+        throw new Error(
+          `Timed out after ${timeout}ms waiting for condition to be true`
+        );
+      }
+
+      return cy.request(requestConfig).then((response) => {
+        // If predicate returns true, we're done
+        if (predicate(response)) {
+          return response;
+        }
+
+        // Otherwise wait and try again
+        cy.wait(interval);
+        return attempt();
+      });
+    };
+
+    return attempt();
+  }
+);
+
+Cypress.Commands.add(
+  'registerAgent',
+  ({ flowTemplateJSON, agentName, type }) => {
+    cy.request(
+      'POST',
+      `${BACKEND_BASE_PATH}${FLOW_FRAMEWORK_API.ROOT}`,
+      flowTemplateJSON
+    )
+      .then((resp) => {
+        return cy
+          .request(
+            'POST',
+            `${BACKEND_BASE_PATH}${FLOW_FRAMEWORK_API.PROVISION.replace(
+              '<workflow_id>',
+              resp.body.workflow_id
+            )}`
+          )
+          .then((resp) => {
+            const workflowId = resp.body.workflow_id;
+            provisionedWorkflows.push({ workflowId });
+            return workflowId;
+          });
+      })
+      .then((workflowId) =>
+        cy
+          .requestPollUntil(
+            {
+              method: 'GET',
+              url: `${BACKEND_BASE_PATH}${FLOW_FRAMEWORK_API.ROOT}/${workflowId}/_status?all=true`,
+            },
+            (resp) => {
+              const { state, provisioning_progress: provisioningProgress } =
+                resp.body;
+              return state === 'COMPLETED' && provisioningProgress === 'DONE';
+            }
+          )
+          .then((resp) => {
+            const { resources_created: resourcesCreated } = resp.body;
+            const agentResource = resourcesCreated.find(
+              ({
+                workflow_step_id: workflowStepId,
+                resource_type: resourceType,
+              }) => resourceType === 'agent_id' && workflowStepId === agentName
+            );
+            if (!agentResource) {
+              return new Error(
+                `Unable to find agent for ${agentName} in workflow ${workflowId}`
+              );
+            }
+            const agentId = agentResource.resource_id;
+            provisionedWorkflows
+              .filter((workflow) => workflow.workflowId === workflowId)
+              .forEach((workflow) => {
+                workflow.agentName = agentName;
+              });
+            return agentId;
+          })
+      )
+      .then((agentId) => cy.putAgentIdConfig({ type, agentName, agentId }));
+  }
+);
+
+Cypress.Commands.add('putAgentIdConfig', ({ type, agentName, agentId }) => {
   // When enabling the DATASOURCE-MANAGEment-ENABLED flag, we need to config the root agent ID in a no auth data source.
   if (
     Cypress.env('SECURITY_ENABLED') &&
@@ -142,14 +167,20 @@ Cypress.Commands.add('putRootAgentId', (agentId) => {
   ) {
     // The .plugins-ml-config index is a system index and need to call the API by using certificate file
     return cy.exec(
-      `curl -k --cert <(cat <<EOF \n${certPublicKeyContent}\nEOF\n) --key <(cat <<EOF\n${certPrivateKeyContent}\nEOF\n) -XPUT '${BACKEND_BASE_PATH}${ML_COMMONS_API.UPDATE_ROOT_AGENT_CONFIG}'  -H 'Content-Type: application/json' -d '{"type":"os_chat_root_agent","configuration":{"agent_id":"${agentId}"}}'`
+      `curl -k --cert <(cat <<EOF \n${certPublicKeyContent}\nEOF\n) --key <(cat <<EOF\n${certPrivateKeyContent}\nEOF\n) -XPUT '${BACKEND_BASE_PATH}${ML_COMMONS_API.ML_CONFIG_DOC.replace(
+        '<agent_name>',
+        agentName
+      )}'  -H 'Content-Type: application/json' -d '{"type":"os_chat_root_agent","configuration":{"agent_id":"${agentId}"}}'`
     );
   } else {
     return cy.request(
       'PUT',
-      `${BACKEND_BASE_PATH}${ML_COMMONS_API.UPDATE_ROOT_AGENT_CONFIG}`,
+      `${BACKEND_BASE_PATH}${ML_COMMONS_API.ML_CONFIG_DOC.replace(
+        '<agent_name>',
+        agentName
+      )}`,
       {
-        type: 'os_chat_root_agent',
+        type,
         configuration: {
           agent_id: agentId,
         },
@@ -158,19 +189,50 @@ Cypress.Commands.add('putRootAgentId', (agentId) => {
   }
 });
 
-Cypress.Commands.add('cleanRootAgent', () => {
-  return;
+Cypress.Commands.add('deleteAgentConfig', ({ agentName }) => {
+  // When enabling the DATASOURCE-MANAGEment-ENABLED flag, we need to config the root agent ID in a no auth data source.
+  if (
+    Cypress.env('SECURITY_ENABLED') &&
+    !Cypress.env('DATASOURCE_MANAGEMENT_ENABLED')
+  ) {
+    // The .plugins-ml-config index is a system index and need to call the API by using certificate file
+    return cy.exec(
+      `curl -k --cert <(cat <<EOF \n${certPublicKeyContent}\nEOF\n) --key <(cat <<EOF\n${certPrivateKeyContent}\nEOF\n) -XPUT '${BACKEND_BASE_PATH}${ML_COMMONS_API.ML_CONFIG_DOC.replace(
+        '<agent_name>',
+        agentName
+      )}'  -H 'Content-Type: application/json'`
+    );
+  } else {
+    return cy.request(
+      'DELETE',
+      `${BACKEND_BASE_PATH}${ML_COMMONS_API.ML_CONFIG_DOC.replace(
+        '<agent_name>',
+        agentName
+      )}`
+    );
+  }
+});
+
+Cypress.Commands.add('cleanProvisionedAgents', () => {
+  for (let i = 0; i < provisionedWorkflows.length; i++) {
+    const workflow = provisionedWorkflows[i];
+    cy.request(
+      'POST',
+      `${BACKEND_BASE_PATH}${FLOW_FRAMEWORK_API.DEPROVISION.replace(
+        '<workflow_id>',
+        workflow.workflowId
+      )}`
+    );
+    if (workflow.agentName) {
+      cy.deleteAgentConfig({
+        agentName: workflow.agentName,
+      });
+    }
+  }
   /**
-   * TODO wait for flow framework to be built into snapshot.
+   * wait for 2s
    */
-  // cy.request(
-  //   'POST',
-  //   `${BACKEND_BASE_PATH}${FLOW_FRAMEWORK_API.CREATE_FLOW_TEMPLATE}/${usingWorkflowId}/_deprovision`
-  // );
-  // /**
-  //  * wait for 2s
-  //  */
-  // cy.wait(2000);
+  cy.wait(2000);
 });
 
 Cypress.Commands.add('startDummyServer', () => {
--- cypress/utils/plugins/dashboards-assistant/constants.js
+++ cypress/utils/plugins/dashboards-assistant/constants.js
@@ -3,19 +3,25 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-// export const WORKFLOW_API_PREFIX = '/_plugins/_flow_framework/workflow';
+export const WORKFLOW_API_PREFIX = '/_plugins/_flow_framework/workflow';
 export const ML_COMMONS_API_PREFIX = '/_plugins/_ml';
 
-// export const FLOW_FRAMEWORK_API = {
-//   CREATE_FLOW_TEMPLATE: WORKFLOW_API_PREFIX,
-// };
+export const FLOW_FRAMEWORK_API = {
+  ROOT: WORKFLOW_API_PREFIX,
+  PROVISION: `${WORKFLOW_API_PREFIX}/<workflow_id>/_provision`,
+  DEPROVISION: `${WORKFLOW_API_PREFIX}/<workflow_id>/_deprovision`,
+};
+
+export const ASSISTANT_AGENT_NAME = {
+  CHAT: 'os_chat',
+};
 
 export const ML_COMMONS_API = {
   CREATE_CONNECTOR: `${ML_COMMONS_API_PREFIX}/connectors/_create`,
   CREATE_MODEL: `${ML_COMMONS_API_PREFIX}/models/_register`,
   CREATE_AGENT: `${ML_COMMONS_API_PREFIX}/agents/_register`,
-  UPDATE_ROOT_AGENT_CONFIG: `/.plugins-ml-config/_doc/os_chat`,
-  AGENT_CONFIG: `${ML_COMMONS_API_PREFIX}/config/os_chat`,
+  ML_CONFIG_DOC: '/.plugins-ml-config/_doc/<agent_name>',
+  AGENT_CONFIG: `${ML_COMMONS_API_PREFIX}/config/<agent_name>`,
 };
 
 export const ASSISTANT_API_BASE = '/api/assistant';
