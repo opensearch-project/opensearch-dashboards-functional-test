a48f8ee50b8130bed80c0bc6be11d63dc23982b4
Kaituo Li <kaituo@amazon.com>
2025-07-21 14:55:31 -0700

    forecasting IT (#1811)
    * forecasting IT
    
    Signed-off-by: Kaituo Li <kaituo@amazon.com>
    
    * address comments
    
    Signed-off-by: Kaituo Li <kaituo@amazon.com>
    
    ---------
    
    Signed-off-by: Kaituo Li <kaituo@amazon.com>

--- cypress/utils/plugins/anomaly-detection-dashboards-plugin/commands.js
+++ cypress/utils/plugins/anomaly-detection-dashboards-plugin/commands.js
@@ -10,7 +10,10 @@ import {
   getADDeleteDetectorNodeApiPath,
   getADStopDetectorApiPath,
   getADGetDetectorApiPath,
+  getForecastStopForecasterNodeApiPath,
+  FORECAST_NODE_API_PATH,
 } from '../../constants';
+import { selectTopItemFromFilter } from './helpers';
 
 Cypress.Commands.add(
   'mockGetDetectorOnAction',
@@ -108,3 +111,139 @@ Cypress.Commands.add('stopDetector', (detectorId) => {
     `${Cypress.env('openSearchUrl')}/${getADStopDetectorApiPath(detectorId)}`
   );
 });
+
+/**
+ * Custom command to perform the full forecaster creation flow
+ * from the 'Define forecaster' step onwards.
+ * @param {object} forecasterDetails - The details for the new forecaster.
+ * @param {string} forecasterDetails.name - The name of the forecaster.
+ * @param {string} forecasterDetails.index - The source index.
+ * @param {string} forecasterDetails.timestampField - The timestamp field name.
+ * @param {boolean} [forecasterDetails.categoricalField=false] - Whether to use a categorical field.
+ * @param {string} forecasterDetails.featureField - The field to forecast.
+ * @param {boolean} [forecasterDetails.test=false] - Whether to create and test the forecaster.
+ * @param {number} [forecasterDetails.interval=0] - The interval of the forecaster.
+ */
+Cypress.Commands.add('createForecaster', (forecasterDetails) => {
+  const {
+    name,
+    index,
+    timestampField,
+    featureField,
+    categoricalField = false,
+    test = false,
+    interval = 0,
+  } = forecasterDetails;
+
+  // pop up for tenant selection
+  // Handles the optional "Select your tenant" pop-up
+  cy.get('body').then(($body) => {
+    // We look for an element containing "Select your tenant" to avoid being
+    // specific about which tag (e.g. h1, h2) is used for the title.
+    if ($body.find(':contains("Select your tenant")').length > 0) {
+      const confirmButton = $body.find('button:contains("Confirm")');
+      if (confirmButton.length) {
+        cy.wrap(confirmButton.first()).click();
+      }
+    }
+  });
+
+  cy.getElementByTestId('defineOrEditForecasterTitle').should('exist');
+  cy.getElementByTestId('forecasterNameTextInput').type(name);
+  cy.getElementByTestId('indicesFilter').type(`${index}{enter}`);
+
+  // Type the timestamp field directly
+  cy.getElementByTestId('timestampFilter').type(`${timestampField}{enter}`);
+
+  // Type the feature field name and then the field to forecast
+  cy.getElementByTestId('featureNameTextInput-0').type(featureField);
+  cy.getElementByTestId('featureFieldTextInput-0').type(
+    `${featureField}{enter}`
+  );
+
+  if (categoricalField) {
+    // Wait for the checkbox to be visible and enabled before clicking
+    cy.get('label[for="categoryFieldCheckbox"]').click();
+
+    // Select the first item from the category field combo box
+    selectTopItemFromFilter('categoryFieldComboBox', true);
+  }
+
+  // Try to click next button and handle potential validation errors
+  cy.getElementByTestId('defineForecasterNextButton').click();
+
+  cy.getElementByTestId('defineOrEditForecasterTitle').should('not.exist');
+  cy.getElementByTestId('configureOrEditModelConfigurationTitle').should(
+    'exist'
+  );
+
+  cy.getElementByTestId('suggestParametersButton').click();
+  cy.getElementByTestId('suggestParametersDialogTitle').should('exist');
+  cy.getElementByTestId('generateSuggestionsButton').click();
+  cy.getElementByTestId('suggestedParametersResult').should('exist');
+
+  cy.getElementByTestId('useSuggestedParametersButton').click();
+
+  // The dialog should close and we're back on the model configuration page
+  cy.getElementByTestId('suggestParametersDialogTitle').should('not.exist');
+
+  if (interval > 0) {
+    cy.get('input[name="interval"]').clear().type(interval);
+  }
+
+  if (test) {
+    cy.getElementByTestId('createTestForecasterButton').click();
+  } else {
+    cy.getElementByTestId('createForecasterButton').click();
+  }
+});
+
+/**
+ * Custom command to delete all indices on the remote cluster.
+ */
+Cypress.Commands.add('deleteAllRemoteIndices', () => {
+  const isSecure = Cypress.env('SECURITY_ENABLED');
+  const remoteBaseUrl = isSecure
+    ? Cypress.env('remoteDataSourceBasicAuthUrl')
+    : Cypress.env('remoteDataSourceNoAuthUrl');
+
+  if (!remoteBaseUrl) {
+    cy.log('Remote base URL not configured, skipping remote index deletion.');
+    return;
+  }
+
+  cy.request({
+    method: 'DELETE',
+    url: `${remoteBaseUrl}/*`,
+    failOnStatusCode: false,
+  }).then((response) => {
+    // A 404 is also acceptable if no indices exist to be deleted.
+    expect(response.status).to.be.oneOf([200, 404, 403]);
+  });
+});
+
+Cypress.Commands.add(
+  'mockGetForecastersOnAction',
+  function (fixtureFileName, funcMockedOn) {
+    cy.intercept(FORECAST_NODE_API_PATH.GET_FORECASTERS, {
+      fixture: fixtureFileName,
+    }).as('getForecasters');
+
+    funcMockedOn();
+
+    cy.wait('@getForecasters');
+  }
+);
+
+Cypress.Commands.add(
+  'mockStopForecasterOnAction',
+  function (fixtureFileName, forecasterId, funcMockedOn) {
+    cy.intercept(getForecastStopForecasterNodeApiPath(forecasterId), {
+      fixture: fixtureFileName,
+    }).as('stopForecaster');
+
+    funcMockedOn();
+
+    cy.wait('@stopForecaster');
+  }
+);
