2846dafb37857d275597a22399c40021c48a51ae
yyfamazon <yyf@amazon.com>
2026-03-05 13:50:05 +0800

    Upgrade Cypress to latest version for FTRepo
    Signed-off-by: yyfamazon <yyf@amazon.com>

--- cypress/integration/core-opensearch-dashboards/opensearch-dashboards/dashboard_sanity_test_spec.js
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import { dashboardSanityTests } from '../../common/dashboard_sample_data_spec.js';
-
-dashboardSanityTests();
