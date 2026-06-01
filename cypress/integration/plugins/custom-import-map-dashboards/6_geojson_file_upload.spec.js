1843387d010eb392614e4bb15f8f99820f98408b
Heemin Kim <heemin@amazon.com>
2026-06-01 11:07:42 -0700

    Fix Cypress 15 compatibility for custom-import-map-dashboards tests (#2021)
    * Fix map zoom test to use Cypress dblclick() action
    
    MapLibre's DoubleClickZoomHandler requires a full pointer event sequence
    to trigger zoom. Using .trigger('dblclick') only dispatches an isolated
    DOM event which MapLibre ignores. Switch to .dblclick() which simulates
    the complete user interaction sequence.
    
    Signed-off-by: Heemin Kim <heemin@amazon.com>
    
    * Fix file upload test to use Cypress built-in selectFile()
    
    The cypress-file-upload plugin's attachFile() does not work correctly
    with Cypress 15. Replace with the built-in selectFile() command
    available since Cypress 9.3.
    
    Signed-off-by: Heemin Kim <heemin@amazon.com>
    
    ---------
    
    Signed-off-by: Heemin Kim <heemin@amazon.com>

--- cypress/integration/plugins/custom-import-map-dashboards/6_geojson_file_upload.spec.js
+++ cypress/integration/plugins/custom-import-map-dashboards/6_geojson_file_upload.spec.js
@@ -6,7 +6,6 @@
 import { BASE_PATH } from '../../../utils/constants';
 import { CURRENT_TENANT } from '../../../utils/commands';
 import { MiscUtils } from '@opensearch-dashboards-test/opensearch-dashboards-test-library';
-import 'cypress-file-upload';
 
 const miscUtils = new MiscUtils(cy);
 
@@ -33,8 +32,8 @@ describe('Verify successful custom geojson file upload', () => {
     // Click on "Import Vector Map" tab, which is part of customImportMap plugin
     cy.contains('Import Vector Map').click({ force: true });
 
-    cy.get('[data-testId="filePicker"]').attachFile(
-      'plugins/custom-import-map-dashboards/sample_geo.json'
+    cy.get('[data-testId="filePicker"]').selectFile(
+      'cypress/fixtures/plugins/custom-import-map-dashboards/sample_geo.json'
     );
     cy.get('[data-testId="customIndex"]').type('sample');
     cy.contains('Import file').click({ force: true });
