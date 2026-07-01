34b6d1356ba737fed24cbbf1a3911e120af96fa0
Sumukh Swamy <sumukhhs@amazon.com>
2026-06-30 18:18:16 -0700

    fix(test): fix reports-dashboards edit spec flaky tests (#2086)
    Backport of #2054 to 2.19.
    
    Consolidates test setup into reusable helpers (setupEditIntercepts,
    openEditPage, clickSaveChanges) and injects origin header into PUT
    requests to fix 400 validation errors caused by Chromium omitting
    the Origin header for same-origin requests.
    
    Signed-off-by: sumukhswamy <sumukhhs@amazon.com>

--- cypress/integration/plugins/reports-dashboards/02-edit.spec.js
+++ cypress/integration/plugins/reports-dashboards/02-edit.spec.js
@@ -5,8 +5,60 @@
 
 import { BASE_PATH, TIMEOUT } from '../../../utils/constants';
 
+const REPORT_DEFINITION_API = '**/api/reporting/reportDefinitions/**';
+
+const setupEditIntercepts = () => {
+  cy.intercept('GET', REPORT_DEFINITION_API).as('getReportDefinition');
+
+  cy.intercept('PUT', REPORT_DEFINITION_API, (req) => {
+    req.headers['origin'] = BASE_PATH;
+    if (
+      req.body &&
+      req.body.report_params &&
+      req.body.report_params.core_params
+    ) {
+      req.body.report_params.core_params.origin = BASE_PATH;
+    }
+  }).as('updateReportDefinition');
+};
+
+const openEditPage = () => {
+  cy.get('#reportDefinitionDetailsLink').first().click({ force: true });
+
+  cy.get('#editReportDefinitionButton', { timeout: TIMEOUT }).should('exist');
+  cy.get('#editReportDefinitionButton').click();
+
+  cy.url().should('include', 'edit');
+
+  cy.wait('@getReportDefinition', { timeout: TIMEOUT });
+
+  cy.get('#reportSettingsName', { timeout: TIMEOUT })
+    .should('exist')
+    .and(($el) => {
+      expect($el.val()).to.not.equal('');
+    });
+};
+
+const clickSaveChanges = () => {
+  cy.get('#editReportDefinitionButton')
+    .contains('Save Changes')
+    .trigger('mouseover')
+    .click({ force: true });
+
+  cy.wait('@updateReportDefinition', { timeout: TIMEOUT })
+    .its('response.statusCode')
+    .should('eq', 200);
+
+  // check that re-direct to home page
+  cy.get('#reportDefinitionDetailsLink', { timeout: TIMEOUT }).should('exist');
+};
+
 describe('Cypress', () => {
-  it('Visit edit page, update name and description', () => {
+  beforeEach(() => {
+    setupEditIntercepts();
+
+    // Wait before visiting to allow index refresh after previous test's save
+    cy.wait(5000);
     cy.visit(`${BASE_PATH}/app/reports-dashboards#/`, {
       waitForGetTenant: true,
     });
@@ -14,195 +66,53 @@ describe('Cypress', () => {
       'include',
       '/reports-dashboards'
     );
-
-    cy.wait(12500);
-
-    cy.intercept(
-      'GET',
-      `${BASE_PATH}/api/reporting/getReportSource/dashboard`
-    ).as('dashboard');
-
-    cy.intercept(
-      'GET',
-      `${BASE_PATH}/api/reporting/getReportSource/visualization`
-    ).as('visualization');
-
-    cy.intercept('GET', `${BASE_PATH}/api/reporting/getReportSource/search`).as(
-      'search'
+    // Retry: if the list doesn't load, reload the page
+    cy.get('body').then(($body) => {
+      if ($body.find('#reportDefinitionDetailsLink').length === 0) {
+        cy.wait(5000);
+        cy.reload();
+      }
+    });
+    cy.get('#reportDefinitionDetailsLink', { timeout: TIMEOUT }).should(
+      'exist'
     );
+  });
 
-    cy.intercept('GET', `${BASE_PATH}/api/observability/notebooks/`).as(
-      'notebook'
-    );
-
-    cy.intercept(
-      'GET',
-      `${BASE_PATH}/api/observability/notebooks/savedNotebook`
-    ).as('savedNotebook');
-
-    cy.get('#reportDefinitionDetailsLink').first().click({ force: true });
-
-    cy.get('#editReportDefinitionButton').should('exist');
-
-    cy.get('#editReportDefinitionButton').click();
-
-    cy.url().should('include', 'edit');
-
-    cy.wait(1000);
-    cy.wait('@dashboard');
-    cy.wait('@visualization');
-    cy.wait('@search');
-    cy.wait('@notebook');
-    cy.wait('@savedNotebook');
+  it('Visit edit page, update name and description', () => {
+    openEditPage();
 
     // update the report name
-    cy.get('#reportSettingsName')
-      .click({ force: true })
-      .type('{selectall}{backspace} update name');
+    cy.get('#reportSettingsName').type('{selectall}{backspace} update name');
 
     // update report description
     cy.get('#reportSettingsDescription').type(
       '{selectall}{backspace} update description'
     );
 
-    cy.get('#editReportDefinitionButton')
-      .contains('Save Changes')
-      .trigger('mouseover')
-      .click({ force: true });
-
-    cy.wait(12500);
-
-    // check that re-direct to home page
-    cy.get('#reportDefinitionDetailsLink').should('exist');
+    clickSaveChanges();
   });
 
   it('Visit edit page, change report trigger', () => {
-    cy.visit(`${BASE_PATH}/app/reports-dashboards#/`, {
-      waitForGetTenant: true,
-    });
-    cy.location('pathname', { timeout: TIMEOUT }).should(
-      'include',
-      '/reports-dashboards'
-    );
+    openEditPage();
 
-    cy.wait(12500);
-
-    cy.intercept(
-      'GET',
-      `${BASE_PATH}/api/reporting/getReportSource/dashboard`
-    ).as('dashboard');
-
-    cy.intercept(
-      'GET',
-      `${BASE_PATH}/api/reporting/getReportSource/visualization`
-    ).as('visualization');
-
-    cy.intercept('GET', `${BASE_PATH}/api/reporting/getReportSource/search`).as(
-      'search'
-    );
-
-    cy.intercept('GET', `${BASE_PATH}/api/observability/notebooks/`).as(
-      'notebook'
-    );
-
-    cy.intercept(
-      'GET',
-      `${BASE_PATH}/api/observability/notebooks/savedNotebook`
-    ).as('savedNotebook');
-
-    cy.get('#reportDefinitionDetailsLink').first().click();
-
-    cy.get('#editReportDefinitionButton').should('exist');
-
-    cy.get('#editReportDefinitionButton').click();
-
-    cy.url().should('include', 'edit');
-
-    cy.wait(1000);
-
-    cy.wait('@dashboard');
-    cy.wait('@visualization');
-    cy.wait('@search');
-    cy.wait('@notebook');
-    cy.wait('@savedNotebook');
     cy.get('#reportDefinitionTriggerTypes > div:nth-child(2)').click({
       force: true,
     });
 
-    cy.get('#Schedule').check({ force: true });
-    cy.get('#editReportDefinitionButton')
-      .contains('Save Changes')
-      .trigger('mouseover')
-      .click({ force: true });
+    cy.get('#Schedule').check({ force: true }).should('be.checked');
 
-    cy.wait(12500);
-
-    // check that re-direct to home page
-    cy.get('#reportDefinitionDetailsLink').should('exist');
+    clickSaveChanges();
   });
 
   it('Visit edit page, change report trigger back', () => {
-    cy.visit(`${BASE_PATH}/app/reports-dashboards#/`, {
-      waitForGetTenant: true,
-    });
-    cy.location('pathname', { timeout: TIMEOUT }).should(
-      'include',
-      '/reports-dashboards'
-    );
-
-    cy.wait(12500);
-
-    cy.intercept(
-      'GET',
-      `${BASE_PATH}/api/reporting/getReportSource/dashboard`
-    ).as('dashboard');
-
-    cy.intercept(
-      'GET',
-      `${BASE_PATH}/api/reporting/getReportSource/visualization`
-    ).as('visualization');
-
-    cy.intercept('GET', `${BASE_PATH}/api/reporting/getReportSource/search`).as(
-      'search'
-    );
-
-    cy.intercept('GET', `${BASE_PATH}/api/observability/notebooks/`).as(
-      'notebook'
-    );
-
-    cy.intercept(
-      'GET',
-      `${BASE_PATH}/api/observability/notebooks/savedNotebook`
-    ).as('savedNotebook');
-
-    cy.get('#reportDefinitionDetailsLink').first().click();
-
-    cy.get('#editReportDefinitionButton').should('exist');
-
-    cy.get('#editReportDefinitionButton').click();
-
-    cy.url().should('include', 'edit');
-
-    cy.wait(1000);
-
-    cy.wait('@dashboard');
-    cy.wait('@visualization');
-    cy.wait('@search');
-    cy.wait('@notebook');
-    cy.wait('@savedNotebook');
+    openEditPage();
 
     cy.get('#reportDefinitionTriggerTypes > div:nth-child(1)').click({
       force: true,
     });
 
-    cy.get('#editReportDefinitionButton')
-      .contains('Save Changes')
-      .trigger('mouseover')
-      .click({ force: true });
+    cy.get('#On\\ demand').check({ force: true }).should('be.checked');
 
-    cy.wait(12500);
-
-    // check that re-direct to home page
-    cy.get('#reportDefinitionDetailsLink').should('exist');
+    clickSaveChanges();
   });
 });
